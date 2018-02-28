f0621c17b8cbeba590d2fdf535bf8f2c5a41faaa
Unknown <info@mandli.sk>
2018-02-28 16:14:15 +0100

    add cors

--- server/Startup.cs
+++ server/Startup.cs
@@ -23,12 +23,22 @@ namespace server
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options
+                            =>
+                                options.AddPolicy("CorsPolicy",
+                                    b => b.AllowAnyOrigin()
+                                        .AllowAnyMethod()
+                                        .AllowAnyHeader()
+                                        .AllowCredentials()));
+
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors("CorsPolicy");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
