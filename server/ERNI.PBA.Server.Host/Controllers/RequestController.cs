e6ffaf93205b65120ef2d14718d01aa989554dd5
Unknown <Andrej.Mandli@erni.sk>
2018-05-23 01:09:40 +0200

    patxh and put reuests

--- server/ERNI.PBA.Server.Host/Controllers/RequestController.cs
+++ server/ERNI.PBA.Server.Host/Controllers/RequestController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using ERNI.PBA.Server.DataAccess;
 using ERNI.PBA.Server.DataAccess.Repository;
 using ERNI.PBA.Server.Host.Examples;
+using ERNI.PBA.Server.Host.Model;
 using ERNI.PBA.Server.Host.Model.PendingRequests;
 using ERNI.PBA.Server.Utils;
 using Microsoft.AspNetCore.Authorization;
@@ -19,11 +20,13 @@ namespace server.Controllers
     public class RequestController : Controller
     {
         private readonly IRequestRepository _requestRepository;
+        private readonly IUserRepository _userRepository;
         private readonly IUnitOfWork _unitOfWork;
 
-        public RequestController(IRequestRepository requestRepository, IUnitOfWork unitOfWork)
+        public RequestController(IRequestRepository requestRepository, IUserRepository userRepository, IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
+            _userRepository = userRepository;
             _requestRepository = requestRepository;
         }
 
@@ -81,6 +84,40 @@ namespace server.Controllers
             return Ok();
         }
 
+        [HttpPut]
+        public async Task<IActionResult> PutRequest([FromBody]PutRequestModel payload, CancellationToken cancellationToken)
+        {
+            var request = new Request
+            {
+                UserId = 1,
+                Year = 2018,
+                Title = payload.Title,
+                Amount = payload.Amount,
+                Date = payload.Date,
+                State = RequestState.Pending
+            };
+
+            _requestRepository.AddRequest(request);
+
+            await _unitOfWork.SaveChanges(cancellationToken);
+
+            return Ok();
+        }
+
+        [HttpPatch("{id}")]
+        public async Task<IActionResult> PatchRequest(int id, [FromBody]PatchRequestModel payload, CancellationToken cancellationToken)
+        {
+            var request = await _requestRepository.GetRequest(id, cancellationToken);
+
+            request.Title = payload.Title;
+            request.Amount = payload.Amount;
+            request.Date = payload.Date;
+            request.State = RequestState.Pending;
+
+            await _unitOfWork.SaveChanges(cancellationToken);
+
+            return Ok();
+        }
 
         [HttpGet("pending")]
         [SwaggerResponseExample(200, typeof(PendingRequestExample))]
@@ -95,6 +132,7 @@ namespace server.Controllers
                     Title = _.Title,
                     Amount = _.Amount,
                     Year = _.Year,
+                    Date = _.Date,
                     User = new ERNI.PBA.Server.Host.Model.PendingRequests.UserModel
                     {
                         Id = _.UserId,
---
+++ server/ERNI.PBA.Server.Host/Model/PutRequestModel.cs
@@ -0,0 +1,11 @@
+namespace ERNI.PBA.Server.Host.Model
+{
+    public class PutRequestModel
+    {
+        public System.DateTime Date { get; set; }
+
+        public string Title { get; set; }
+
+        public decimal Amount { get; set; }
+    }
+}
