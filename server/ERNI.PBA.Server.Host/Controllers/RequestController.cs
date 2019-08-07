b70bfa49993406d9e523c01ba5d5623ee94e217e
Dominik Pastierovič <pado@erni.sk>
2019-08-07 09:00:49 +0200

    Added TimeZone convertion for request date

--- server/ERNI.PBA.Server.Host/Controllers/RequestController.cs
+++ server/ERNI.PBA.Server.Host/Controllers/RequestController.cs
@@ -192,7 +192,7 @@ namespace server.Controllers
                 Year = currentYear,
                 Title = payload.Title,
                 Amount = payload.Amount,
-                Date = payload.Date,
+                Date = payload.Date.ToLocalTime().Date,
                 State = RequestState.Pending,
                 CategoryId = payload.Category.Id,
                 Url = payload.Url
@@ -260,7 +260,7 @@ namespace server.Controllers
             request.Title = payload.Title;
             request.Amount = payload.Amount;
             request.CategoryId = payload.CategoryId;
-            request.Date = payload.Date;
+            request.Date = payload.Date.ToLocalTime().Date;
             request.Url = payload.Url;
 
             await _unitOfWork.SaveChanges(cancellationToken);
