f6947f38e143ea511d84751513f185f881b52223
Dominik Pastierovič <dpastierovic@gmail.com>
2019-08-12 11:39:37 +0200

    Code cleanup

--- server/ERNI.PBA.Server.Host/Startup.cs
+++ server/ERNI.PBA.Server.Host/Startup.cs
@@ -143,7 +143,7 @@ namespace ERNI.PBA.Server
 
             app.UseAuthentication();
 
-            //app.UseQuartz();
+            app.UseQuartz();
 
             app.UseSwagger();
             app.UseSwaggerUI(c =>
