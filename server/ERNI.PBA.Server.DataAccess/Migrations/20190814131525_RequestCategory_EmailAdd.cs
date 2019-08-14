1a644a909fcf50ede9bef080aa5b6bdded1b851f
lukino563 <lukino563@gmail.com>
2019-08-14 15:25:09 +0200

    DB Migration for RequestCategory

---
+++ server/ERNI.PBA.Server.DataAccess/Migrations/20190814131525_RequestCategory_EmailAdd.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ERNI.PBA.Server.DataAccess.Migrations
+{
+    public partial class RequestCategory_EmailAdd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "RequestCategories",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "RequestCategories");
+        }
+    }
+}
