e2dd7d8f64d6876c21c5e15ca15cd1595cb633be
Andrej Mandli <andrej.mandli@erni.sk>
2020-01-05 21:04:53 +0100

    _d

--- server/ERNI.PBA.Server.Host/Model/PostRequestModel.cs
+++ server/ERNI.PBA.Server.Host/Model/PostRequestModel.cs
@@ -10,8 +10,10 @@ namespace ERNI.PBA.Server.Host.Model
 
         public decimal Amount { get; set; }
 
-        public RequestCategory Category { get; set; }
+        public int CategoryId { get; set; }
 
         public string Url { get; set; }
+
+        public int BudgetId { get; set; }
     }
 }
