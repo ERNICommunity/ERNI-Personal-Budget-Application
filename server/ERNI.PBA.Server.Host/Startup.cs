30f00febb0e8d6c810a5818c86c3a327c3d989e0
Emil Kuhejda <emil.kuhejda@gmail.com>
2020-04-02 08:02:48 +0200

    Fix.

--- server/ERNI.PBA.Server.Host/Startup.cs
+++ server/ERNI.PBA.Server.Host/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.Filters;
@@ -147,7 +148,7 @@ namespace ERNI.PBA.Server.Host
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env) // , ILogger<Startup> logger)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) // , ILogger<Startup> logger)
         {
             app.UseCors("CorsPolicy");
 
