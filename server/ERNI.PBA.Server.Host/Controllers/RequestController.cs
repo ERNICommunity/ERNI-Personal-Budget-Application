260c9449c7d68f1f686594716f08c807486b69e5
Emil Kuhejda <emil.kuhejda@gmail.com>
2020-04-06 13:01:20 +0200

    Code refactoring.

--- server/ERNI.PBA.Server.Host/Controllers/RequestController.cs
+++ server/ERNI.PBA.Server.Host/Controllers/RequestController.cs
@@ -1,17 +1,12 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
-using ERNI.PBA.Server.DataAccess;
 using ERNI.PBA.Server.DataAccess.Model;
-using ERNI.PBA.Server.DataAccess.Repository;
 using ERNI.PBA.Server.Host.Commands.Requests;
 using ERNI.PBA.Server.Host.Model;
 using ERNI.PBA.Server.Host.Model.PendingRequests;
+using ERNI.PBA.Server.Host.Queries.Budgets;
 using ERNI.PBA.Server.Host.Queries.Requests;
-using ERNI.PBA.Server.Host.Services;
 using ERNI.PBA.Server.Host.Utils;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
@@ -24,23 +19,10 @@ namespace ERNI.PBA.Server.Host.Controllers
     [Authorize]
     public class RequestController : Controller
     {
-        private readonly IRequestRepository _requestRepository;
-        private readonly IUserRepository _userRepository;
-        private readonly IBudgetRepository _budgetRepository;
-        private readonly IUnitOfWork _unitOfWork;
         private readonly IMediator _mediator;
 
-        public RequestController(
-            IRequestRepository requestRepository,
-            IUserRepository userRepository,
-            IBudgetRepository budgetRepository,
-            IUnitOfWork unitOfWork,
-            IMediator mediator)
+        public RequestController(IMediator mediator)
         {
-            _unitOfWork = unitOfWork;
-            _userRepository = userRepository;
-            _requestRepository = requestRepository;
-            _budgetRepository = budgetRepository;
             _mediator = mediator;
         }
 
@@ -230,70 +212,28 @@ namespace ERNI.PBA.Server.Host.Controllers
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteRequest(int id, CancellationToken cancellationToken)
         {
-            var request = await _requestRepository.GetRequest(id, cancellationToken);
-
-            if (request == null)
+            var deleteRequestCommand = new DeleteRequestCommand
             {
-                return BadRequest("Not a valid id");
-            }
+                Principal = HttpContext.User,
+                RequestId = id
+            };
 
-            var user = HttpContext.User;
-            if (!user.IsInRole(Roles.Admin) && user.GetId() != request.UserId)
-            {
-                return BadRequest("Access denied");
-            }
-
-            await _requestRepository.DeleteRequest(request);
-
-            await _unitOfWork.SaveChanges(cancellationToken);
+            await _mediator.Send(deleteRequestCommand, cancellationToken);
 
             return Ok();
         }
 
         [HttpGet("budget-left/{amount}/{year}")]
         [Authorize(Roles = Roles.Admin)]
-#pragma warning disable SA1202 // Elements should be ordered by access
         public async Task<UserModel[]> BudgetLeft(decimal amount, int year, CancellationToken cancellationToken)
-#pragma warning restore SA1202 // Elements should be ordered by access
         {
-            var budgetAmount = (await _budgetRepository.GetTotalAmountsByYear(year, cancellationToken))
-                .ToDictionary(_ => _.BudgetId, _ => _.Amount);
-
-            var budgets = await _budgetRepository.GetBudgets(year, BudgetTypeEnum.PersonalBudget, cancellationToken);
-
-            var users = (await _userRepository.GetAllUsers(cancellationToken))
-                .ToDictionary(_ => _.Id);
-
-            var usersWithBudgetLeft = new List<UserModel>();
-
-            foreach (var budget in budgets)
+            var getBudgetLeftQuery = new GetBudgetLeftQuery
             {
-                if (budgetAmount[budget.Id] + amount <= budget.Amount)
-                {
-                    var user = users[budget.UserId];
+                Amount = amount,
+                Year = year
+            };
 
-                    usersWithBudgetLeft.Add(new UserModel
-                    {
-                        Id = user.Id,
-                        FirstName = user.FirstName,
-                        IsAdmin = user.IsAdmin,
-                        IsSuperior = user.IsSuperior,
-                        IsViewer = user.IsViewer,
-                        LastName = user.LastName,
-                        State = user.State,
-                        Superior = user.Superior != null ? new SuperiorModel
-                        {
-                            FirstName = user.Superior.FirstName,
-                            Id = user.Superior.Id,
-                            LastName = user.Superior.LastName
-                        }
-                        :
-                        null
-                    });
-                }
-            }
-
-            return usersWithBudgetLeft.ToArray();
+            return await _mediator.Send(getBudgetLeftQuery, cancellationToken);
         }
     }
 }
---
+++ server/ERNI.PBA.Server.Host/Handlers/Requests/DeleteRequestHandler.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using ERNI.PBA.Server.DataAccess;
+using ERNI.PBA.Server.DataAccess.Repository;
+using ERNI.PBA.Server.Host.Commands.Requests;
+using ERNI.PBA.Server.Host.Exceptions;
+using ERNI.PBA.Server.Host.Utils;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+
+namespace ERNI.PBA.Server.Host.Handlers.Requests
+{
+    public class DeleteRequestHandler : IRequestHandler<DeleteRequestCommand, bool>
+    {
+        private readonly IRequestRepository _requestRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteRequestHandler(
+            IRequestRepository requestRepository,
+            IUnitOfWork unitOfWork)
+        {
+            _requestRepository = requestRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<bool> Handle(DeleteRequestCommand command, CancellationToken cancellationToken)
+        {
+            var request = await _requestRepository.GetRequest(command.RequestId, cancellationToken);
+            if (request == null)
+            {
+                throw new OperationErrorException(StatusCodes.Status400BadRequest, "Not a valid id");
+            }
+
+            if (!command.Principal.IsInRole(Roles.Admin) && command.Principal.GetId() != request.UserId)
+            {
+                throw new OperationErrorException(StatusCodes.Status400BadRequest, "Access denied");
+            }
+
+            await _requestRepository.DeleteRequest(request);
+
+            await _unitOfWork.SaveChanges(cancellationToken);
+
+            return true;
+        }
+    }
+}
