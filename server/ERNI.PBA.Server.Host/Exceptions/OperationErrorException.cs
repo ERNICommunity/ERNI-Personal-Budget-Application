29e70a7adcaeff1cb63d3ae548a9a275a3b665c7
Emil Kuhejda <emil.kuhejda@gmail.com>
2020-04-09 07:16:57 +0200

    Code review findings.

--- server/ERNI.PBA.Server.Host/Exceptions/OperationErrorException.cs
+++ server/ERNI.PBA.Server.Host/Exceptions/OperationErrorException.cs
@@ -1,15 +1,9 @@
 ﻿using System;
-using Microsoft.AspNetCore.Http;
 
 namespace ERNI.PBA.Server.Host.Exceptions
 {
     public class OperationErrorException : Exception
     {
-        public OperationErrorException(string message)
-            : this(StatusCodes.Status400BadRequest, message)
-        {
-        }
-
         public OperationErrorException(int httpStatusCode)
         {
             HttpStatusCode = httpStatusCode;
