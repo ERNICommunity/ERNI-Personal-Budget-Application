b03e32a53cada0ac0a2b2aab38957fc651c4e16e
Lubomir Maron <lubomir.maron@erni.sk>
2020-05-12 09:17:54 +0200

    83947 show list of images in requests deatail

--- client/src/app/requests/requestDetail/requestDetail.component.ts
+++ client/src/app/requests/requestDetail/requestDetail.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, ViewChild } from '@angular/core';
 import { ActivatedRoute, Params} from '@angular/router';
 import { Location } from '@angular/common';
 import { Request } from '../../model/request/request';
 import { RequestService } from '../../services/request.service';
 import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { InvoiceImageService } from '../../services/invoice-image.service';
 
 @Component({
   selector: 'app-request-detail',
@@ -12,23 +13,35 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['requestDetail.component.css']
 })
 export class RequestDetailComponent implements OnInit {
+  @ViewChild('downloadLink',{static : false}) downloadLink: ElementRef;
   request: Request;
   selectedDate : Date;
   requestForm: FormGroup;
   httpResponseError : string;
+  images: [number, string][];
   
   constructor(private requestService: RequestService,
               private route: ActivatedRoute,
               private location: Location,
-              public modal: NgbActiveModal){
+              public modal: NgbActiveModal,
+              private invoiceImageService: InvoiceImageService){
                }
 
   ngOnInit() {
-    // this.route.params.subscribe((params: Params) => {
-    //   var idParam = params['requestId']; 
-      
-    //   this.getRequest(idParam);
-    // });
+  }
+
+  download(imageId: number, imageName: string) {
+    this.invoiceImageService.getInvoiceImage(imageId).subscribe(blob => { this.processBlob(blob, imageName); })
+  }
+
+  processBlob(blob: Blob, name: string) {
+    let fileObject = new File([blob], name);
+    let url = window.URL.createObjectURL(fileObject);
+    let link = this.downloadLink.nativeElement;
+    link.setAttribute('download', name);
+    link.href = url;
+    link.click();
+    window.URL.revokeObjectURL(url);
   }
 
   public getRequest(id: number): void {
@@ -37,6 +50,9 @@ export class RequestDetailComponent implements OnInit {
         { 
           this.request = request;
           this.selectedDate = new Date(request.date);
+          this.invoiceImageService.getInvoiceImages(id).subscribe(names => {
+            this.images = names;
+          });
         },err => {
           this.httpResponseError = err.error
         });
