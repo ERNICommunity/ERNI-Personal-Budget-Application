bc964b3a62a50d0efcd0a3fe53d2e9c5046e6aa0
Andrej Mandli <andrej.mandli@erni.sk>
2021-01-31 16:57:36 +0100

    Endpoint for API version

---
+++ server/ERNI.PBA.Server.Host/Controllers/VersionController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERNI.PBA.Server.Host.Controllers
+{
+    [Route("api/[controller]")]
+    public class VersionController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(GetType().Assembly.GetName().Version.ToString());
+        }
+    }
+}
