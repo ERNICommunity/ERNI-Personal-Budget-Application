b184deb6dfded31004ad9eb83a2055d61df77a11
Andrej Mandli <andrej.mandli@erni.sk>
2021-03-14 13:48:00 +0100

    Adjust rule severities

--- server/ERNI.PBA.Server.Domain/Enums/BudgetTypeEnum.cs
+++ server/ERNI.PBA.Server.Domain/Enums/BudgetTypeEnum.cs
@@ -1,7 +1,9 @@
 ﻿namespace ERNI.PBA.Server.Domain.Enums
 {
 #pragma warning disable CA1008 // Enums should have zero value
+#pragma warning disable CA1711 // Identifiers should not have incorrect suffix
     public enum BudgetTypeEnum
+#pragma warning restore CA1711 // Identifiers should not have incorrect suffix
 #pragma warning restore CA1008 // Enums should have zero value
     {
         PersonalBudget = 1,
