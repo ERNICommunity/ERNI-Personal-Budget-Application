4b95c42c37f40265eb9c3e69cbb7327e5c1700b1
Vladimir Cernega <vladimir.cernega@betterask.erni>
2021-03-31 19:23:41 +0200

    Add invoiced amount and rejected date with migration

--- server/ERNI.PBA.Server.DataAccess/Migrations/20210331172151_AddInvoicedAmountAndRejectedDate.Designer.cs
+++ server/ERNI.PBA.Server.DataAccess/Migrations/20210331172151_AddInvoicedAmountAndRejectedDate.Designer.cs
@@ -4,14 +4,16 @@ using ERNI.PBA.Server.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ERNI.PBA.Server.DataAccess.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20210331172151_AddInvoicedAmountAndRejectedDate")]
+    partial class AddInvoicedAmountAndRejectedDate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ server/ERNI.PBA.Server.DataAccess/Migrations/20210331172151_AddInvoicedAmountAndRejectedDate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ERNI.PBA.Server.DataAccess.Migrations
+{
+    public partial class AddInvoicedAmountAndRejectedDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "InvoicedAmount",
+                table: "Requests",
+                type: "decimal(18,2)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RejectedDate",
+                table: "Requests",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InvoicedAmount",
+                table: "Requests");
+
+            migrationBuilder.DropColumn(
+                name: "RejectedDate",
+                table: "Requests");
+        }
+    }
+}
