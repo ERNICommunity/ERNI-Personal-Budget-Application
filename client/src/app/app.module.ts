414484f4029c457be5520b407ee4d101f6f5f4cd
Andrej Mandli <andrej.mandli@erni.sk>
2021-04-20 20:09:00 +0200

    Move msal-related code out of appModule

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,182 +1,134 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { rootRouterConfig } from './app.routes';
-import { ConfigService } from './services/config.service';
-import { RequestService } from './services/request.service';
-import { HttpClientModule } from '@angular/common/http';
-import { UserService } from './services/user.service';
-import { NgbModule, NgbAlert } from '@ng-bootstrap/ng-bootstrap';
-import { BudgetService } from './services/budget.service';
-import { FormsModule } from '@angular/forms';
-import { ServiceHelper } from './services/service.helper';
-import { RequestAddComponent } from './requests/requestAdd/requestAdd.component';
-import { RequestEditComponent } from './requests/requestEdit/requestEdit.component';
-import { RequestDetailComponent } from './requests/requestDetail/requestDetail.component';
-import { RequestMassComponent } from './requests/requestMass/requestMass.component';
-import { OtherBudgetsComponent } from './budgets/otherBudgets/otherBudgets.component';
-import { OtherBudgetsDetailComponent } from './budgets/otherBudgetsDetail/otherBudgetsDetail.component';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
-import { BusyIndicatorService } from './services/busy-indicator.service';
-import { ViewerGuard } from './services/guards/viewer.guard';
-import { AlertComponent } from './directives/alert/alert.component';
-import { NewRequestModalComponent } from './requests/requestAdd/newRequestModal.component';
-import { DataChangeNotificationService } from './services/dataChangeNotification.service';
-import { FileUploadComponent } from './file-upload/file-upload.component';
-import { InvoiceImageService } from './services/invoice-image.service';
-import { TeamBudgetService } from './services/team-budget.service';
-import { ExportService } from './services/export.service';
-import { WizardModule } from 'primeng-extensions-wizard/components/wizard.module';
-import { StepsModule } from 'primeng/steps';
-import { requestCreateReducer } from './requests/state/request.reducer'
-import { requestApproveReducer } from './requests/state/request.reducer'
-import { requestInvoiceReducer } from './requests/state/request.reducer'
-import { requestRejectReducer } from './requests/state/request.reducer'
+import { AppComponent } from "./app.component";
+import { RouterModule } from "@angular/router";
+import { LoginComponent } from "./login/login.component";
+import { rootRouterConfig } from "./app.routes";
+import { ConfigService } from "./services/config.service";
+import { RequestService } from "./services/request.service";
+import { HttpClientModule } from "@angular/common/http";
+import { UserService } from "./services/user.service";
+import { NgbModule, NgbAlert } from "@ng-bootstrap/ng-bootstrap";
+import { BudgetService } from "./services/budget.service";
+import { FormsModule } from "@angular/forms";
+import { ServiceHelper } from "./services/service.helper";
+import { RequestAddComponent } from "./requests/requestAdd/requestAdd.component";
+import { RequestEditComponent } from "./requests/requestEdit/requestEdit.component";
+import { RequestDetailComponent } from "./requests/requestDetail/requestDetail.component";
+import { RequestMassComponent } from "./requests/requestMass/requestMass.component";
+import { OtherBudgetsComponent } from "./budgets/otherBudgets/otherBudgets.component";
+import { OtherBudgetsDetailComponent } from "./budgets/otherBudgetsDetail/otherBudgetsDetail.component";
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
+import { ReactiveFormsModule } from "@angular/forms";
+import { BusyIndicatorService } from "./services/busy-indicator.service";
+import { ViewerGuard } from "./services/guards/viewer.guard";
+import { AlertComponent } from "./directives/alert/alert.component";
+import { NewRequestModalComponent } from "./requests/requestAdd/newRequestModal.component";
+import { DataChangeNotificationService } from "./services/dataChangeNotification.service";
+import { FileUploadComponent } from "./file-upload/file-upload.component";
+import { InvoiceImageService } from "./services/invoice-image.service";
+import { TeamBudgetService } from "./services/team-budget.service";
+import { ExportService } from "./services/export.service";
+import { WizardModule } from "primeng-extensions-wizard/components/wizard.module";
+import { StepsModule } from "primeng/steps";
+import { requestCreateReducer } from "./requests/state/request.reducer";
+import { requestApproveReducer } from "./requests/state/request.reducer";
+import { requestInvoiceReducer } from "./requests/state/request.reducer";
+import { requestRejectReducer } from "./requests/state/request.reducer";
 
 import {
-    MsalInterceptor,
-    MsalInterceptorConfiguration,
-    MsalGuardConfiguration,
-    MSAL_INSTANCE,
-    MSAL_GUARD_CONFIG,
-    MSAL_INTERCEPTOR_CONFIG,
+  MsalInterceptor,
+  MSAL_INSTANCE,
+  MSAL_GUARD_CONFIG,
+  MSAL_INTERCEPTOR_CONFIG,
+  MsalService,
+  MsalGuard,
+  MsalBroadcastService,
+} from "@azure/msal-angular";
+
+import { AdminRoleGuard } from "./services/guards/admin-role.guard";
+import { AuthenticationService } from "./services/authentication.service";
+import { StoreModule } from "@ngrx/store";
+import { MyBudgetModule } from "./my-budget/my-budget.module";
+import { RequestsModule } from "./requests/requests.module";
+import {
+  MSALGuardConfigFactory,
+  MSALInstanceFactory,
+  MSALInterceptorConfigFactory,
+} from "./utils/msal";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RequestAddComponent,
+    RequestDetailComponent,
+    RequestEditComponent,
+    RequestMassComponent,
+    OtherBudgetsComponent,
+    OtherBudgetsDetailComponent,
+    AlertComponent,
+    NewRequestModalComponent,
+    FileUploadComponent,
+  ],
+  imports: [
+    NgbModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserModule,
+    RouterModule.forRoot(rootRouterConfig),
+    ReactiveFormsModule,
+    MyBudgetModule,
+    RequestsModule,
+    StepsModule,
+    StoreModule.forRoot({}, {}),
+    StoreModule.forFeature("request", { requestCreateReducer }),
+    StoreModule.forFeature("request", { requestApproveReducer }),
+    StoreModule.forFeature("request", { requestRejectReducer }),
+    StoreModule.forFeature("request", { requestInvoiceReducer }),
+  ],
+  entryComponents: [
+    NewRequestModalComponent,
+    RequestDetailComponent,
+    RequestEditComponent,
+  ],
+  providers: [
+    ConfigService,
+    ServiceHelper,
+    ExportService,
+    RequestService,
+    UserService,
+    BudgetService,
+    TeamBudgetService,
+    BusyIndicatorService,
+    DataChangeNotificationService,
+    InvoiceImageService,
+    ViewerGuard,
+    AuthenticationService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true,
+    },
+    {
+      provide: MSAL_INSTANCE,
+      useFactory: MSALInstanceFactory,
+    },
+    {
+      provide: MSAL_GUARD_CONFIG,
+      useFactory: MSALGuardConfigFactory,
+    },
+    {
+      provide: MSAL_INTERCEPTOR_CONFIG,
+      useFactory: MSALInterceptorConfigFactory,
+    },
     MsalService,
     MsalGuard,
     MsalBroadcastService,
-  } from '@azure/msal-angular';
-
-  import {
-    BrowserCacheLocation,
-    InteractionType,
-    IPublicClientApplication,
-    LogLevel,
-    PublicClientApplication,
-  } from '@azure/msal-browser';
-  import { environment } from '../environments/environment';
-  import { AdminRoleGuard } from './services/guards/admin-role.guard';
-  import { AuthenticationService } from './services/authentication.service';
-  import { StoreModule } from '@ngrx/store';
-  import { MyBudgetModule } from './my-budget/my-budget.module';
-  import { RequestsModule } from './requests/requests.module';
-
-  export function MSALInstanceFactory(): IPublicClientApplication {
-    return new PublicClientApplication({
-      auth: {
-        clientId: environment.clientId,
-        redirectUri: environment.msalLoginRedirectUri,
-        postLogoutRedirectUri: environment.msalLogoutRedirectUri,
-        authority: 'https://login.microsoftonline.com/eb25818e-5bd5-49bf-99de-53e3e7b42630'
-      },
-      cache: {
-        cacheLocation: BrowserCacheLocation.LocalStorage,
-        storeAuthStateInCookie: false
-      },
-      system: {
-        loggerOptions: {
-          logLevel: LogLevel.Verbose,
-          loggerCallback: (level: LogLevel, message: string, containsPii: boolean) => {
-            if (level === LogLevel.Error) {
-              console.error(message);
-            }
-          },
-          piiLoggingEnabled: false
-        }
-      }
-    });
-  }
-
-  export function MSALInterceptorConfigFactory(): MsalInterceptorConfiguration {
-    const protectedResourceMap = new Map<string, Array<string>>();
-
-    Object
-      .keys(environment.protectedResourceMap)
-      .forEach(key => protectedResourceMap
-      .set(key, environment.protectedResourceMap[key]));
-
-    return {
-      interactionType: InteractionType.Popup,
-      protectedResourceMap,
-    };
-  }
-
-  export function MSALGuardConfigFactory(): MsalGuardConfiguration {
-    return { interactionType: InteractionType.Popup };
-  }
-
-@NgModule({
-    declarations: [
-        AppComponent,
-        LoginComponent,
-        RequestAddComponent,
-        RequestDetailComponent,
-        RequestEditComponent,
-        RequestMassComponent,
-        OtherBudgetsComponent,
-        OtherBudgetsDetailComponent,
-        AlertComponent,
-        NewRequestModalComponent,
-        FileUploadComponent
-    ],
-    imports: [
-        NgbModule,
-        FormsModule,
-        HttpClientModule,
-        BrowserModule,
-        RouterModule.forRoot(rootRouterConfig),
-        ReactiveFormsModule,
-        MyBudgetModule,
-        RequestsModule,
-        StepsModule,
-        StoreModule.forRoot({}, {}),
-        StoreModule.forFeature('request', { requestCreateReducer }),
-        StoreModule.forFeature('request', { requestApproveReducer }),
-        StoreModule.forFeature('request', { requestRejectReducer }),
-        StoreModule.forFeature('request', { requestInvoiceReducer })
-    ],
-    entryComponents: [
-        NewRequestModalComponent, RequestDetailComponent, RequestEditComponent
-    ],
-    providers: [
-        ConfigService,
-        ServiceHelper,
-        ExportService,
-        RequestService,
-        UserService,
-        BudgetService,
-        TeamBudgetService,
-        BusyIndicatorService,
-        DataChangeNotificationService,
-        InvoiceImageService,
-        ViewerGuard,
-        AuthenticationService,
-        {
-            provide: HTTP_INTERCEPTORS,
-            useClass: MsalInterceptor,
-            multi: true,
-          },
-          {
-            provide: MSAL_INSTANCE,
-            useFactory: MSALInstanceFactory,
-          },
-          {
-            provide: MSAL_GUARD_CONFIG,
-            useFactory: MSALGuardConfigFactory,
-          },
-          {
-            provide: MSAL_INTERCEPTOR_CONFIG,
-            useFactory: MSALInterceptorConfigFactory,
-          },
-          MsalService,
-          MsalGuard,
-          MsalBroadcastService,
-          AdminRoleGuard,
-          WizardModule
-    ],
-    bootstrap: [AppComponent]
+    AdminRoleGuard,
+    WizardModule,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
---
+++ client/src/app/utils/msal.ts
@@ -0,0 +1,53 @@
+import { MsalGuardConfiguration, MsalInterceptorConfiguration } from '@azure/msal-angular';
+import {
+  BrowserCacheLocation,
+  InteractionType,
+  IPublicClientApplication,
+  LogLevel,
+  PublicClientApplication,
+} from '@azure/msal-browser';
+import { environment } from "../../environments/environment";
+
+export function MSALInstanceFactory(): IPublicClientApplication {
+  return new PublicClientApplication({
+    auth: {
+      clientId: environment.clientId,
+      redirectUri: environment.msalLoginRedirectUri,
+      postLogoutRedirectUri: environment.msalLogoutRedirectUri,
+      authority: 'https://login.microsoftonline.com/eb25818e-5bd5-49bf-99de-53e3e7b42630'
+    },
+    cache: {
+      cacheLocation: BrowserCacheLocation.LocalStorage,
+      storeAuthStateInCookie: false
+    },
+    system: {
+      loggerOptions: {
+        logLevel: LogLevel.Verbose,
+        loggerCallback: (level: LogLevel, message: string, containsPii: boolean) => {
+          if (level === LogLevel.Error) {
+            console.error(message);
+          }
+        },
+        piiLoggingEnabled: false
+      }
+    }
+  });
+}
+
+export function MSALInterceptorConfigFactory(): MsalInterceptorConfiguration {
+  const protectedResourceMap = new Map<string, Array<string>>();
+
+  Object
+    .keys(environment.protectedResourceMap)
+    .forEach(key => protectedResourceMap
+    .set(key, environment.protectedResourceMap[key]));
+
+  return {
+    interactionType: InteractionType.Popup,
+    protectedResourceMap,
+  };
+}
+
+export function MSALGuardConfigFactory(): MsalGuardConfiguration {
+  return { interactionType: InteractionType.Popup };
+}
