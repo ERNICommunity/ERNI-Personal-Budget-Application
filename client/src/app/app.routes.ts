4e4210b8e3c2876e0be027afd4304e15b65078af
Andrej Mandli <andrej.mandli@erni.sk>
2021-04-21 08:27:35 +0200

    Remove unused route

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,14 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { RequestAddComponent } from './requests/requestAdd/requestAdd.component';
 import { RequestMassComponent } from './requests/requestMass/requestMass.component';
 import { AdminRoleGuard } from './services/guards/admin-role.guard';
-import { MsalGuard } from '@azure/msal-angular';
 
 export const rootRouterConfig: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
-    { path: 'create-request', component: RequestAddComponent, canActivate: [MsalGuard] },
     { path: 'mass-request', component: RequestMassComponent, canActivate: [AdminRoleGuard] }
 ];
 
