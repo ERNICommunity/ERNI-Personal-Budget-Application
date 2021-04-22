21e7845d25e07b5a05e5cc6e91ebd5a408060345
Andrej Mandli <andrej.mandli@erni.sk>
2021-04-22 22:40:32 +0200

    Component for editing basic request info
    _basic

---
+++ client/src/app/requests/requestEdit/basic-request-info-editor/basic-request-info-editor.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BasicRequestInfoEditorComponent } from './basic-request-info-editor.component';
+
+describe('BasicRequestInfoEditorComponent', () => {
+  let component: BasicRequestInfoEditorComponent;
+  let fixture: ComponentFixture<BasicRequestInfoEditorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BasicRequestInfoEditorComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BasicRequestInfoEditorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
