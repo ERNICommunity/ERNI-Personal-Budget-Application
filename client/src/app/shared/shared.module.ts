5017bbe9490e9b1c5c2ea51d5df5c0908b3e2d89
Andrej Mandli <andrej.mandli@erni.sk>
2021-04-22 20:38:28 +0200

    Extend shared module

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,19 +1,29 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { AlertComponent } from './alert/alert.component';
-import { FileUploadComponent } from './file-upload/file-upload.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { AlertComponent } from "./alert/alert.component";
+import { FileUploadComponent } from "./file-upload/file-upload.component";
+import { DividerModule } from "primeng/divider";
+import { CalendarModule } from "primeng/calendar";
+import { FormsModule } from "@angular/forms";
+import { InputTextareaModule } from "primeng/inputtextarea";
+import { InputTextModule } from "primeng/inputtext";
+import { InputNumberModule } from "primeng/inputnumber";
+import { ProgressSpinnerModule } from "primeng/progressspinner";
 
 @NgModule({
-  declarations: [
+  declarations: [AlertComponent, FileUploadComponent],
+  imports: [CommonModule, FormsModule, DividerModule],
+  exports: [
+    CommonModule,
+    FormsModule,
+    DividerModule,
+    CalendarModule,
+    InputTextModule,
+    InputTextareaModule,
+    InputNumberModule,
+    ProgressSpinnerModule,
     AlertComponent,
     FileUploadComponent,
   ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    AlertComponent,
-    FileUploadComponent
-  ]
 })
-export class SharedModule { }
+export class SharedModule {}
