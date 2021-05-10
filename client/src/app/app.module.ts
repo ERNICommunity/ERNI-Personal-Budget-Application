5c5c2da5f73dfe37a1cd92957ecf3cc1ffccebc3
Andrej Mandli <andrej.mandli@erni.sk>
2021-05-10 09:18:47 +0200

    Get rid of unused code
    _unused

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -13,7 +13,6 @@ import { NgbModule, NgbAlert } from "@ng-bootstrap/ng-bootstrap";
 import { BudgetService } from "./services/budget.service";
 import { FormsModule } from "@angular/forms";
 import { ServiceHelper } from "./services/service.helper";
-import { RequestAddComponent } from "./requests/requestAdd/requestAdd.component";
 import { RequestEditComponent } from "./requests/requestEdit/requestEdit.component";
 import { RequestDetailComponent } from "./requests/requestDetail/requestDetail.component";
 import { RequestMassComponent } from "./requests/requestMass/requestMass.component";
@@ -21,7 +20,6 @@ import { HTTP_INTERCEPTORS } from "@angular/common/http";
 import { ReactiveFormsModule } from "@angular/forms";
 import { BusyIndicatorService } from "./services/busy-indicator.service";
 import { ViewerGuard } from "./services/guards/viewer.guard";
-import { NewRequestModalComponent } from "./requests/requestAdd/newRequestModal.component";
 import { DataChangeNotificationService } from "./services/dataChangeNotification.service";
 import { InvoiceImageService } from "./services/invoice-image.service";
 import { TeamBudgetService } from "./services/team-budget.service";
@@ -55,11 +53,9 @@ import { SharedModule } from "./shared/shared.module";
   declarations: [
     AppComponent,
     LoginComponent,
-    RequestAddComponent,
     RequestDetailComponent,
     RequestEditComponent,
     RequestMassComponent,
-    NewRequestModalComponent,
   ],
   imports: [
     NgbModule,
@@ -76,7 +72,6 @@ import { SharedModule } from "./shared/shared.module";
     TeamBudgetModule
   ],
   entryComponents: [
-    NewRequestModalComponent,
     RequestDetailComponent,
     RequestEditComponent,
   ],
