3d6a40b25eb463356ad8c41787f85d3ebc2f0a24
Andrej Mandli <andrej.mandli@erni.sk>
2021-05-10 12:04:24 +0200

    Reload team requests on save

--- client/src/app/team-budget/team-request/team-request.component.ts
+++ client/src/app/team-budget/team-request/team-request.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { combineLatest } from 'rxjs';
 import { TeamRequestModel } from '../../model/request/teamRequestModel';
+import { DataChangeNotificationService } from '../../services/dataChangeNotification.service';
 import { TeamBudgetService } from '../../services/team-budget.service';
 
 @Component({
@@ -12,11 +14,10 @@ export class TeamRequestComponent implements OnInit {
 
   requests: TeamRequestModel[];
 
-  constructor(private route: ActivatedRoute, private teamBudgetService: TeamBudgetService) { }
+  constructor(private route: ActivatedRoute, private teamBudgetService: TeamBudgetService, private notificationService: DataChangeNotificationService) { }
 
   async ngOnInit(): Promise<void> {
-
-    this.route.params.subscribe(async params => {
+    combineLatest([ this.route.params, this.notificationService.notifications$]).subscribe(async ([params, _]) => {
       this.requests = await this.teamBudgetService.getTeamRequests(Number(params['year']));
     });
   }
