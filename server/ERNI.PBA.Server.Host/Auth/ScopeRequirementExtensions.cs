ceb2ca33b9234fd29db3233c22cca6f7c6c95146
Andrej Mandli <andrej.mandli@betterask.erni>
2021-10-25 19:35:28 +0200

    Quickfix

--- server/ERNI.PBA.Server.Host/Auth/ScopeRequirementExtensions.cs
+++ server/ERNI.PBA.Server.Host/Auth/ScopeRequirementExtensions.cs
@@ -4,9 +4,9 @@ namespace ERNI.PBA.Server.Host.Auth
 {
     public static class ScopeRequirementExtensions
     {
-        public static AuthorizationPolicyBuilder RequireScope(
-            this AuthorizationPolicyBuilder authorizationPolicyBuilder,
-            params string[] requiredScopes) =>
-            authorizationPolicyBuilder.AddRequirements(new ScopePolicyRequirement(requiredScopes));
+        //public static AuthorizationPolicyBuilder RequireScope(
+        //    this AuthorizationPolicyBuilder authorizationPolicyBuilder,
+        //    params string[] requiredScopes) =>
+        //    authorizationPolicyBuilder.AddRequirements(new ScopePolicyRequirement(requiredScopes));
     }
 }
