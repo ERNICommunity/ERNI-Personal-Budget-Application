153144f187e5ebe7676622a02aa05139c7a6eb68
Andrej Mandli <andrej.mandli@betterask.erni>
2022-01-09 19:51:08 +0100

    Change redirect url

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,10 +1,12 @@
 export const environment = {
-  production: true,
-  apiBaseUrl: 'https://ernipbaserver.azurewebsites.net/api/',
-  protectedResourceMap: {
-    'https://ernipbaserver.azurewebsites.net/**': ['api://536ddfb7-294a-4065-9be5-1c580d86fd42/pba_client']
-  },
-  msalLoginRedirectUri: 'https://ernipbaclient.azurewebsites.net',
-  msalLogoutRedirectUri: 'https://ernipbaclient.azurewebsites.net',
-  clientId: '106aed90-25b3-4d81-bbc0-9093d97668d9'
+    production: true,
+    apiBaseUrl: 'https://ernipbaserver.azurewebsites.net/api/',
+    protectedResourceMap: {
+        'https://ernipbaserver.azurewebsites.net/**': [
+            'api://536ddfb7-294a-4065-9be5-1c580d86fd42/pba_client'
+        ]
+    },
+    msalLoginRedirectUri: 'https://pba.erninet.ch',
+    msalLogoutRedirectUri: 'https://pba.erninet.ch',
+    clientId: '106aed90-25b3-4d81-bbc0-9093d97668d9'
 };
