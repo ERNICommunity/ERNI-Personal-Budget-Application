932591456fd1721e217739acae04c66896e6a68c
Andrej Mandli <andrej.mandli@betterask.erni>
2022-03-04 21:49:25 +0100

    Update code

--- client/src/app/services/statistics.service.ts
+++ client/src/app/services/statistics.service.ts
@@ -1,31 +1,35 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { ConfigService } from "./config.service";
-import { isError } from "util";
-import { ServiceHelper } from "./service.helper";
-import { StatisticsModel } from "../model/statisticsModel";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ConfigService } from './config.service';
+import { ServiceHelper } from './service.helper';
+import { StatisticsModel } from '../model/statisticsModel';
 
 @Injectable()
 export class StatisticsService {
-  serviceUrl = "statistics";
+    serviceUrl = 'statistics';
 
-  constructor(
-    private http: HttpClient,
-    private serviceHelper: ServiceHelper,
-    private configService: ConfigService
-  ) {}
+    constructor(
+        private http: HttpClient,
+        private serviceHelper: ServiceHelper,
+        private configService: ConfigService
+    ) {}
 
-  public getStatistics(year: number): Promise<StatisticsModel> {
-    // return new Promise(() => {
-    //   budgets: [
-    //     {
-    //       budgetType: 0,
-    //       totalAmount: 23090,
-    //       totalSpentAmount: 15389,
-    //     },
-    //   ];
-    // });
+    public getStatistics(year: number): Promise<StatisticsModel> {
+        // return new Promise(() => {
+        //   budgets: [
+        //     {
+        //       budgetType: 0,
+        //       totalAmount: 23090,
+        //       totalSpentAmount: 15389,
+        //     },
+        //   ];
+        // });
 
-    return this.http.get<StatisticsModel>(`${this.configService.apiUrlBase}${this.serviceUrl}/${year}`, this.serviceHelper.getHttpOptions()).toPromise();
-  }
+        return this.http
+            .get<StatisticsModel>(
+                `${this.configService.apiUrlBase}${this.serviceUrl}/${year}`,
+                this.serviceHelper.getHttpOptions()
+            )
+            .toPromise();
+    }
 }
