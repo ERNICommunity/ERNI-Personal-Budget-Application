30f0b43b3c834713fe4be6ae871992c6f7eaff33
Andrej Mandli <andrej.mandli@betterask.erni>
2022-03-07 20:28:37 +0100

    Prettier

--- client/src/app/services/request.service.ts
+++ client/src/app/services/request.service.ts
@@ -13,72 +13,172 @@ import { InvoicedAmount } from '../model/invoicedAmount';
 
 @Injectable()
 export class RequestService {
+    requestUrl = 'Request/';
 
-    requestUrl = "Request/";
+    constructor(
+        private http: HttpClient,
+        private serviceHelper: ServiceHelper,
+        private configService: ConfigService
+    ) {}
 
-    constructor(private http: HttpClient, private serviceHelper: ServiceHelper, private configService: ConfigService) { }
-
-    public getPendingRequests(year: number, budgetTypeId: number): Observable<Request[]> {
-        return this.http.get<Request[]>(this.configService.apiUrlBase + this.requestUrl + year + '/state/pending/type/' + budgetTypeId, this.serviceHelper.getHttpOptions())
+    public getPendingRequests(
+        year: number,
+        budgetTypeId: number
+    ): Observable<Request[]> {
+        return this.http.get<Request[]>(
+            this.configService.apiUrlBase +
+                this.requestUrl +
+                year +
+                '/state/pending/type/' +
+                budgetTypeId,
+            this.serviceHelper.getHttpOptions()
+        );
     }
 
-    public getApprovedRequests(year: number, budgetTypeId: number): Observable<Request[]> {
-        return this.http.get<Request[]>(this.configService.apiUrlBase + this.requestUrl + year + '/state/approved/type/' + budgetTypeId, this.serviceHelper.getHttpOptions())
+    public getApprovedRequests(
+        year: number,
+        budgetTypeId: number
+    ): Observable<Request[]> {
+        return this.http.get<Request[]>(
+            this.configService.apiUrlBase +
+                this.requestUrl +
+                year +
+                '/state/approved/type/' +
+                budgetTypeId,
+            this.serviceHelper.getHttpOptions()
+        );
     }
 
-    public getRejectedRequests(year: number, budgetTypeId: number): Observable<Request[]> {
-        return this.http.get<Request[]>(this.configService.apiUrlBase + this.requestUrl + year + '/state/rejected/type/' + budgetTypeId, this.serviceHelper.getHttpOptions())
+    public getRejectedRequests(
+        year: number,
+        budgetTypeId: number
+    ): Observable<Request[]> {
+        return this.http.get<Request[]>(
+            this.configService.apiUrlBase +
+                this.requestUrl +
+                year +
+                '/state/rejected/type/' +
+                budgetTypeId,
+            this.serviceHelper.getHttpOptions()
+        );
     }
 
-    public getCompletedRequests(year: number, budgetTypeId: number): Observable<Request[]> {
-      return this.http.get<Request[]>(this.configService.apiUrlBase + this.requestUrl + year + '/state/completed/type/' + budgetTypeId, this.serviceHelper.getHttpOptions())
+    public getCompletedRequests(
+        year: number,
+        budgetTypeId: number
+    ): Observable<Request[]> {
+        return this.http.get<Request[]>(
+            this.configService.apiUrlBase +
+                this.requestUrl +
+                year +
+                '/state/completed/type/' +
+                budgetTypeId,
+            this.serviceHelper.getHttpOptions()
+        );
     }
 
     public getRequest(id): Observable<Request> {
-        return this.http.get<Request>(this.configService.apiUrlBase + this.requestUrl + id, this.serviceHelper.getHttpOptions())
+        return this.http.get<Request>(
+            this.configService.apiUrlBase + this.requestUrl + id,
+            this.serviceHelper.getHttpOptions()
+        );
     }
 
     public approveRequest(id: number): Observable<Request> {
-        return this.http.post<Request>(this.configService.apiUrlBase + this.requestUrl + id + '/approve', this.serviceHelper.getHttpOptions())
+        return this.http.post<Request>(
+            this.configService.apiUrlBase + this.requestUrl + id + '/approve',
+            this.serviceHelper.getHttpOptions()
+        );
     }
 
     public rejectRequest(id: number): Observable<Request> {
-        return this.http.post<Request>(this.configService.apiUrlBase + this.requestUrl + id + '/reject', this.serviceHelper.getHttpOptions())
+        return this.http.post<Request>(
+            this.configService.apiUrlBase + this.requestUrl + id + '/reject',
+            this.serviceHelper.getHttpOptions()
+        );
     }
 
     public completeRequest(id: number): Promise<Request> {
-        return this.http.post<Request>(this.configService.apiUrlBase + this.requestUrl + id + '/complete', this.serviceHelper.getHttpOptions()).toPromise();
+        return this.http
+            .post<Request>(
+                this.configService.apiUrlBase +
+                    this.requestUrl +
+                    id +
+                    '/complete',
+                this.serviceHelper.getHttpOptions()
+            )
+            .toPromise();
     }
 
     public addRequest(request: NewRequest): Observable<any> {
-        return this.http.post(this.configService.apiUrlBase + this.requestUrl, request, this.serviceHelper.getHttpOptions());
+        return this.http.post(
+            this.configService.apiUrlBase + this.requestUrl,
+            request,
+            this.serviceHelper.getHttpOptions()
+        );
     }
 
     public addTeamRequest(request: NewRequest): Observable<any> {
-        return this.http.post(this.configService.apiUrlBase + this.requestUrl + 'team', request, this.serviceHelper.getHttpOptions());
+        return this.http.post(
+            this.configService.apiUrlBase + this.requestUrl + 'team',
+            request,
+            this.serviceHelper.getHttpOptions()
+        );
     }
 
     public addMassRequest(request: RequestMass): Observable<any> {
-        return this.http.post<RequestMass>(this.configService.apiUrlBase + this.requestUrl + 'mass', request, this.serviceHelper.getHttpOptions());
+        return this.http.post<RequestMass>(
+            this.configService.apiUrlBase + this.requestUrl + 'mass',
+            request,
+            this.serviceHelper.getHttpOptions()
+        );
     }
 
     public updateRequest(request: PatchRequest): Observable<any> {
-        return this.http.put(this.configService.apiUrlBase + this.requestUrl, request, this.serviceHelper.getHttpOptions());
+        return this.http.put(
+            this.configService.apiUrlBase + this.requestUrl,
+            request,
+            this.serviceHelper.getHttpOptions()
+        );
     }
 
     public updateTeamRequest(request: PatchRequest): Observable<any> {
-        return this.http.put(this.configService.apiUrlBase + this.requestUrl + 'team', request, this.serviceHelper.getHttpOptions());
+        return this.http.put(
+            this.configService.apiUrlBase + this.requestUrl + 'team',
+            request,
+            this.serviceHelper.getHttpOptions()
+        );
     }
 
     public updateInvoicedAmount(id: number, amount: number): Promise<any> {
-        return this.http.put(this.configService.apiUrlBase + this.requestUrl +  id + "/setAmount", { amount: amount } as InvoicedAmount, this.serviceHelper.getHttpOptions()).toPromise();
+        return this.http
+            .put(
+                this.configService.apiUrlBase +
+                    this.requestUrl +
+                    id +
+                    '/setAmount',
+                { amount: amount } as InvoicedAmount,
+                this.serviceHelper.getHttpOptions()
+            )
+            .toPromise();
     }
 
     public deleteRequest(id: number): Observable<Request> {
-        return this.http.delete<Request>(this.configService.apiUrlBase + this.requestUrl + id, this.serviceHelper.getHttpOptions());
+        return this.http.delete<Request>(
+            this.configService.apiUrlBase + this.requestUrl + id,
+            this.serviceHelper.getHttpOptions()
+        );
     }
 
     public getUsersWithBudgetLeft(request: BudgetLeft): Observable<User[]> {
-        return this.http.get<User[]>(this.configService.apiUrlBase + this.requestUrl + 'budget-left/' + request.amount + '/' + request.year, this.serviceHelper.getHttpOptions());
+        return this.http.get<User[]>(
+            this.configService.apiUrlBase +
+                this.requestUrl +
+                'budget-left/' +
+                request.amount +
+                '/' +
+                request.year,
+            this.serviceHelper.getHttpOptions()
+        );
     }
 }
