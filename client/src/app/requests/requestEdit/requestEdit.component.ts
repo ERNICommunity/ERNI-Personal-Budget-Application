cf9f8d50b94c4f82c88339f9cf8f66c89396603d
Andrej Mandli <andrej.mandli@betterask.erni>
2022-03-07 21:46:03 +0100

    prettier

--- client/src/app/requests/requestEdit/requestEdit.component.ts
+++ client/src/app/requests/requestEdit/requestEdit.component.ts
@@ -33,181 +33,209 @@ export class RequestEditComponent implements OnInit {
 
     public RequestState = RequestApprovalState; // this is required to be possible to use enum in view
 
-    constructor(private requestService: RequestService,
+    constructor(
+        private requestService: RequestService,
         private router: Router,
         private route: ActivatedRoute,
         private alertService: AlertService,
         private dataChangeNotificationService: DataChangeNotificationService,
-        private busyIndicatorService: BusyIndicatorService) { }
+        private busyIndicatorService: BusyIndicatorService
+    ) {}
 
     ngOnInit() {
+        this.isVisible = true;
 
-      this.isVisible = true;
+        this.route.params.subscribe((params: Params) => {
+            this.budgetId = Number(params['budgetId']);
+            this.requestId = Number(params['requestId']);
 
-      this.route.params.subscribe((params: Params) => {
-
-        this.budgetId = Number(params['budgetId']);
-        this.requestId = Number(params['requestId']);
-
-        if (!isNaN(this.budgetId)) {
-
-          console.log("Creating request");
-          this.popupTitle = 'Create new request';
-          this.request = this.createNewRequest();
-          this.newRequest = true;
-
-        } else if (!isNaN(this.requestId)) {
-
-          console.log("Loading request");
-          this.popupTitle = 'Request details';
-          this.loadRequest(this.requestId);
-          this.newRequest = false;
-        } else {
-
-          this.router.navigate(['my-budget']);
-
-        }
-      });
+            if (!isNaN(this.budgetId)) {
+                console.log('Creating request');
+                this.popupTitle = 'Create new request';
+                this.request = this.createNewRequest();
+                this.newRequest = true;
+            } else if (!isNaN(this.requestId)) {
+                console.log('Loading request');
+                this.popupTitle = 'Request details';
+                this.loadRequest(this.requestId);
+                this.newRequest = false;
+            } else {
+                this.router.navigate(['my-budget']);
+            }
+        });
     }
 
-
     private createNewRequest(): Request {
-      var request = new Request();
-      request.date = new Date();
-      request.state = RequestApprovalState.Pending;
-      return request;
+        var request = new Request();
+        request.date = new Date();
+        request.state = RequestApprovalState.Pending;
+        return request;
     }
 
     public loadRequest(requestId: number): void {
         this.requestId = requestId;
         this.requestService.getRequest(requestId).subscribe(
-          (request) => {
-
-            this.request = {
-              amount: request.amount,
-              budget: request.budget,
-              createDate: request.createDate,
-              date: new Date(request.date),
-              id: request.id,
-              state: request.state,
-              title: request.title,
-              user: request.user,
-              invoicedAmount: request.invoicedAmount,
-              invoiceCount: request.invoiceCount
-            } as Request;
-            console.log(request);
-            console.log(this.request);
-
-          },
-          (err) => {
-            this.httpResponseError = err.error;
-          }
+            (request) => {
+                this.request = {
+                    amount: request.amount,
+                    budget: request.budget,
+                    createDate: request.createDate,
+                    date: new Date(request.date),
+                    id: request.id,
+                    state: request.state,
+                    title: request.title,
+                    user: request.user,
+                    invoicedAmount: request.invoicedAmount,
+                    invoiceCount: request.invoiceCount
+                } as Request;
+                console.log(request);
+                console.log(this.request);
+            },
+            (err) => {
+                this.httpResponseError = err.error;
+            }
         );
     }
 
     public async save() {
-      // this.busyIndicatorService.start();
+        // this.busyIndicatorService.start();
 
-      console.log('Saving');
+        console.log('Saving');
 
-      if (this.request.state == RequestApprovalState.Pending) {
-        console.log("Saving basic info");
-        this.saveBasicInfo();
-      } else if (this.request.state == RequestApprovalState.Approved) {
-        await this.updateSpentAmount();
-      }
+        if (this.request.state == RequestApprovalState.Pending) {
+            console.log('Saving basic info');
+            this.saveBasicInfo();
+        } else if (this.request.state == RequestApprovalState.Approved) {
+            await this.updateSpentAmount();
+        }
     }
 
     private saveBasicInfo() {
-      let budgetId = this.budgetId;
-      let id = this.requestId;
-      let title: string = this.request.title;
-      let amount: number = this.request.amount;
-      let date = this.request.date;
+        let budgetId = this.budgetId;
+        let id = this.requestId;
+        let title: string = this.request.title;
+        let amount: number = this.request.amount;
+        let date = this.request.date;
 
         if (this.newRequest) {
-            this.saveNewRequest({ budgetId, title, amount, date } as NewRequest);
+            this.saveNewRequest({
+                budgetId,
+                title,
+                amount,
+                date
+            } as NewRequest);
         } else if (this.request.state == RequestApprovalState.Pending) {
-          console.log('Calling editExistingRequest()');
-            this.editExistingRequest({ id, title, amount, date } as PatchRequest);
+            console.log('Calling editExistingRequest()');
+            this.editExistingRequest({
+                id,
+                title,
+                amount,
+                date
+            } as PatchRequest);
         }
 
         this.dataChangeNotificationService.notify();
     }
 
     private saveNewRequest(payload: NewRequest): void {
+        console.log('Calling saveNewRequest()');
+        console.log(payload);
 
-      console.log('Calling saveNewRequest()');
-      console.log(payload);
+        let request =
+            this.budgetType == BudgetTypeEnum.TeamBudget
+                ? this.requestService.addTeamRequest(payload)
+                : this.requestService.addRequest(payload);
 
-      let request = this.budgetType == BudgetTypeEnum.TeamBudget
-          ? this.requestService.addTeamRequest(payload)
-          : this.requestService.addRequest(payload);
+        request.subscribe(
+            (_) => {
+                this.busyIndicatorService.end();
 
-      request.subscribe((_) => {
-          this.busyIndicatorService.end();
+                this.dataChangeNotificationService.notify();
+                this.alertService.alert(
+                    new Alert({
+                        message: 'Request created successfully',
+                        type: AlertType.Success,
+                        keepAfterRouteChange: true
+                    })
+                );
 
-          this.dataChangeNotificationService.notify();
-          this.alertService.alert(new Alert({ message: "Request created successfully", type: AlertType.Success, keepAfterRouteChange: true }));
-
-          this.router.navigate(['my-budget']);
-      },
-      err => {
-          this.busyIndicatorService.end();
-          this.alertService.error("Error while creating request: " + JSON.stringify(err.error), "addRequestError");
-      });
-  }
-
-  private async updateSpentAmount() {
-    console.log("Saving amount " + this.request.invoicedAmount);
-
-    if (this.request.invoicedAmount) {
-      try {
-        await this.requestService.updateInvoicedAmount(
-          this.request.id,
-          this.request.invoicedAmount
+                this.router.navigate(['my-budget']);
+            },
+            (err) => {
+                this.busyIndicatorService.end();
+                this.alertService.error(
+                    'Error while creating request: ' +
+                        JSON.stringify(err.error),
+                    'addRequestError'
+                );
+            }
         );
-        this.dataChangeNotificationService.notify();
-        this.alertService.alert(
-          new Alert({
-            message: "Spent amount saved",
-            type: AlertType.Success,
-            keepAfterRouteChange: true,
-          })
-        );
-
-        this.router.navigate(["my-budget"]);
-      } catch (error) {
-        this.alertService.error(JSON.stringify(error.error), "addRequestError");
-      }
     }
-  }
 
-  private editExistingRequest(payload: PatchRequest): void {
+    private async updateSpentAmount() {
+        console.log('Saving amount ' + this.request.invoicedAmount);
 
-      let request = this.budgetType == BudgetTypeEnum.TeamBudget
-          ? this.requestService.updateTeamRequest(payload)
-          : this.requestService.updateRequest(payload);
+        if (this.request.invoicedAmount) {
+            try {
+                await this.requestService.updateInvoicedAmount(
+                    this.request.id,
+                    this.request.invoicedAmount
+                );
+                this.dataChangeNotificationService.notify();
+                this.alertService.alert(
+                    new Alert({
+                        message: 'Spent amount saved',
+                        type: AlertType.Success,
+                        keepAfterRouteChange: true
+                    })
+                );
 
-      request.subscribe(() => {
-          this.alertService.alert(new Alert({ message: "Request updated", type: AlertType.Success, keepAfterRouteChange: true }));
-          this.dataChangeNotificationService.notify();
-      },
-          err => {
-              this.alertService.error("Error while creating request: " + JSON.stringify(err.error), "addRequestError");
-          });
-  }
+                this.router.navigate(['my-budget']);
+            } catch (error) {
+                this.alertService.error(
+                    JSON.stringify(error.error),
+                    'addRequestError'
+                );
+            }
+        }
+    }
 
+    private editExistingRequest(payload: PatchRequest): void {
+        let request =
+            this.budgetType == BudgetTypeEnum.TeamBudget
+                ? this.requestService.updateTeamRequest(payload)
+                : this.requestService.updateRequest(payload);
 
-    public trimTitle() : void {
+        request.subscribe(
+            () => {
+                this.alertService.alert(
+                    new Alert({
+                        message: 'Request updated',
+                        type: AlertType.Success,
+                        keepAfterRouteChange: true
+                    })
+                );
+                this.dataChangeNotificationService.notify();
+            },
+            (err) => {
+                this.alertService.error(
+                    'Error while creating request: ' +
+                        JSON.stringify(err.error),
+                    'addRequestError'
+                );
+            }
+        );
+    }
+
+    public trimTitle(): void {
         this.title = this.title.trim();
     }
 
     public onHide(): void {
-      this.router.navigate(['my-budget']);
+        this.router.navigate(['my-budget']);
     }
 
     public close(): void {
-      this.router.navigate(['my-budget']);
+        this.router.navigate(['my-budget']);
     }
 }
