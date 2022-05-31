95d8df34a08e62bee58b4770293fdcac1d4c622d
Andrej Mandli <andrej.mandli@betterask.erni>
2022-05-31 21:39:00 +0200

    Connect to backend

--- server/ERNI.PBA.Client/Program.cs
+++ server/ERNI.PBA.Client/Program.cs
@@ -1,12 +1,25 @@
 using ERNI.PBA.Client;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient("WebAPI",
+        client => client.BaseAddress = new Uri("http://localhost:5001/api")).AddHttpMessageHandler(sp =>
+        {
+            var handler = sp.GetService<AuthorizationMessageHandler>()
+            .ConfigureHandler(
+                 authorizedUrls: new[] { "http://localhost:5001" },
+                 scopes: new[] { "SCOPE" }
+             );
+            return handler;
+        });
+
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>()
+    .CreateClient("WebAPI"));
 
 builder.Services.AddMsalAuthentication(options =>
 {
