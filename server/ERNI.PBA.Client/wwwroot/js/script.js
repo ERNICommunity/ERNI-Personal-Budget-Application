1de46ce0a2d9b39a56d1f1a85091ce24eacdbfeb
Andrej Mandli <andrej.mandli@betterask.erni>
2022-06-02 21:33:03 +0200

    Call blazor from js

--- server/ERNI.PBA.Client/wwwroot/js/script.js
+++ server/ERNI.PBA.Client/wwwroot/js/script.js
@@ -1,7 +1,9 @@
-﻿alert('loading js');
-
-window.foo = {
+﻿window.foo = {
     bar: function() {
-        alert('Bar called');
+        //alert('Bar called');
+        DotNet.invokeMethodAsync("ERNI.PBA.Client", "CallFromJs")
+    },
+    alert: function () {
+        alert('Blazor called js to call blazor to call js');
     }
 }
