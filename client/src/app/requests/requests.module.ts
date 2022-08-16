d7c1859f0e303e0f1a3a25b1a89077b1596d16c0
Andrej Mandli <andrej.mandli@betterask.erni>
2022-08-16 17:29:29 +0200

    start removing bootstrap

--- client/src/app/requests/requests.module.ts
+++ client/src/app/requests/requests.module.ts
@@ -1,61 +1,60 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { RouterModule } from "@angular/router";
-import { RequestsComponent } from "./requests.component";
-import { ViewerGuard } from "../services/guards/viewer.guard";
-import { RequestDetailComponent } from "./requestDetail/requestDetail.component";
-import { RequestEditComponent } from "./requestEdit/requestEdit.component";
-import { MsalGuard } from "@azure/msal-angular";
-import { RequestListComponent } from "./requestList/requestList.component";
-import { FormsModule } from "@angular/forms";
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
-import { SharedModule } from "../shared/shared.module";
-import { BasicRequestInfoEditorComponent } from "./requestEdit/basic-request-info-editor/basic-request-info-editor.component";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { RequestsComponent } from './requests.component';
+import { ViewerGuard } from '../services/guards/viewer.guard';
+import { RequestDetailComponent } from './requestDetail/requestDetail.component';
+import { RequestEditComponent } from './requestEdit/requestEdit.component';
+import { MsalGuard } from '@azure/msal-angular';
+import { RequestListComponent } from './requestList/requestList.component';
+import { FormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
+import { BasicRequestInfoEditorComponent } from './requestEdit/basic-request-info-editor/basic-request-info-editor.component';
 
 @NgModule({
-  declarations: [
-    RequestsComponent,
-    RequestListComponent,
-    BasicRequestInfoEditorComponent,
-  ],
-  exports: [BasicRequestInfoEditorComponent],
-  imports: [
-    CommonModule,
-    FormsModule,
-    NgbModule,
-    SharedModule,
-    RouterModule.forChild([
-      {
-        path: "requests",
-        component: RequestsComponent,
-        canActivate: [ViewerGuard],
-        children: [
-          {
-            path: "",
-            redirectTo:
-              "personal/pending/" + new Date().getFullYear().toString(),
-            pathMatch: "full",
-          },
-          {
-            path: ":budgetType/:requestState/:year",
-            component: RequestListComponent,
-            canActivate: [ViewerGuard],
-            children: [
-              {
-                path: "detail/:requestId",
-                component: RequestDetailComponent,
+    declarations: [
+        RequestsComponent,
+        RequestListComponent,
+        BasicRequestInfoEditorComponent
+    ],
+    exports: [BasicRequestInfoEditorComponent],
+    imports: [
+        CommonModule,
+        FormsModule,
+        SharedModule,
+        RouterModule.forChild([
+            {
+                path: 'requests',
+                component: RequestsComponent,
                 canActivate: [ViewerGuard],
-              },
-              {
-                path: "edit/:id",
-                component: RequestEditComponent,
-                canActivate: [MsalGuard],
-              },
-            ],
-          },
-        ],
-      },
-    ]),
-  ],
+                children: [
+                    {
+                        path: '',
+                        redirectTo:
+                            'personal/pending/' +
+                            new Date().getFullYear().toString(),
+                        pathMatch: 'full'
+                    },
+                    {
+                        path: ':budgetType/:requestState/:year',
+                        component: RequestListComponent,
+                        canActivate: [ViewerGuard],
+                        children: [
+                            {
+                                path: 'detail/:requestId',
+                                component: RequestDetailComponent,
+                                canActivate: [ViewerGuard]
+                            },
+                            {
+                                path: 'edit/:id',
+                                component: RequestEditComponent,
+                                canActivate: [MsalGuard]
+                            }
+                        ]
+                    }
+                ]
+            }
+        ])
+    ]
 })
 export class RequestsModule {}
