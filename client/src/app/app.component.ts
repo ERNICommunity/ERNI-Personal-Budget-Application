a04da942f984387e6b9a166e7432d896be5c730b
Andrej Mandli <andrej.mandli@betterask.erni>
2023-01-16 18:15:20 +0100

    Remove unused stuff

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,8 +1,5 @@
-import { Component, DoCheck, OnInit } from '@angular/core';
-import { UserService } from './services/user.service';
-import { Router, NavigationStart, NavigationCancel, NavigationError, NavigationEnd } from '@angular/router';
+import { Component } from '@angular/core';
 import { BusyIndicatorService } from './services/busy-indicator.service';
-import { User } from './model/user';
 import { AuthenticationService } from './services/authentication.service';
 
 @Component({
@@ -11,31 +8,10 @@ import { AuthenticationService } from './services/authentication.service';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    user: User;
-    initialized: boolean;
-
-    constructor(public authService: AuthenticationService, private userService: UserService, private router: Router, public busyIndicatorService: BusyIndicatorService) {
-        this.initialized = false;
-        this.user = new User();
-
-        this.router.events.subscribe(event => {
-            switch (true) {
-                case event instanceof NavigationStart: {
-                    this.busyIndicatorService.start();
-                    break;
-                }
-                case event instanceof NavigationEnd:
-                case event instanceof NavigationCancel:
-                case event instanceof NavigationError: {
-                    this.busyIndicatorService.end();
-                    break;
-                }
-                default: {
-                    break;
-                }
-            }
-        });
-    }
+    constructor(
+        public authService: AuthenticationService,
+        public busyIndicatorService: BusyIndicatorService
+    ) {}
 
     async logout(): Promise<void> {
         await this.authService.logout();
