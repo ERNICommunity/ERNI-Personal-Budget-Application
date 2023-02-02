e5c4b38e468af22277afe9b39993d7b6d80bede8
Andrej Mandli <andrej.mandli@betterask.erni>
2023-02-02 11:17:48 +0100

    Bundle carlito font with the app

--- server/ERNI.PBA.Server.Host/Program.cs
+++ server/ERNI.PBA.Server.Host/Program.cs
@@ -1,5 +1,8 @@
+using System.Reflection;
 using Autofac.Extensions.DependencyInjection;
 using Azure.Storage.Blobs;
+using ClosedXML.Excel;
+using ClosedXML.Graphics;
 using ERNI.PBA.Server.Business.Commands.Users;
 using ERNI.PBA.Server.DataAccess;
 using ERNI.PBA.Server.DataAccess.Repository;
@@ -16,6 +19,13 @@ namespace ERNI.PBA.Server.Host
         public static void Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
+
+            // Fonts expected by ClosedXML are not available on Linux
+            // we bundle Calibri with the app instead
+            using (var fallbackFontStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("ERNI.PBA.Server.Host.Resources.carlito.Carlito-Regular.ttf"))
+            {
+                LoadOptions.DefaultGraphicEngine = DefaultGraphicEngine.CreateWithFontsAndSystemFonts(fallbackFontStream);
+            }
 
             using (var serviceScope = host.Services.GetService<IServiceScopeFactory>()!.CreateScope())
             {
