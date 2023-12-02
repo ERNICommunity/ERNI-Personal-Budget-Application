f87797ef9b045dfab6f1bf0d494d0fd52f752240
Andrej <ashdr89fpaysd98pfyas@mandli.sk>
2023-12-02 17:33:43 +0100

    Create budgets

--- client/src/app/budgets/create-budgets/create-budgets.component.ts
+++ client/src/app/budgets/create-budgets/create-budgets.component.ts
@@ -1,74 +1,51 @@
-import {
-  Component,
-  Input,
-  OnChanges,
-  OnInit,
-  SimpleChanges,
-} from "@angular/core";
-import { BudgetType } from "../../model/budgetType";
+import { Component, Input, inject } from "@angular/core";
 import { BudgetTypeEnum } from "../../model/budgetTypeEnum";
 import { User } from "../../model/user";
 import { AlertService } from "../../services/alert.service";
 import { BudgetService } from "../../services/budget.service";
 import { DataChangeNotificationService } from "../../services/dataChangeNotification.service";
-import { ButtonModule } from "primeng/button";
-import { SharedModule } from "primeng/api";
-import { DropdownModule } from "primeng/dropdown";
-import { InputNumberModule } from "primeng/inputnumber";
-import { NgIf } from "@angular/common";
-import { InputTextModule } from "primeng/inputtext";
-import { FormsModule } from "@angular/forms";
-import { AlertComponent } from "../../shared/alert/alert.component";
+import { toSignal } from "@angular/core/rxjs-interop";
+import { BehaviorSubject, of, switchMap } from "rxjs";
+import { SharedModule } from "../../shared/shared.module";
 
 @Component({
   selector: "app-create-budgets",
   templateUrl: "./create-budgets.component.html",
   styleUrls: ["./create-budgets.component.css"],
   standalone: true,
-  imports: [
-    AlertComponent,
-    FormsModule,
-    InputTextModule,
-    NgIf,
-    InputNumberModule,
-    DropdownModule,
-    SharedModule,
-    ButtonModule,
-  ],
+  imports: [SharedModule],
 })
-export class CreateBudgetsComponent implements OnInit, OnChanges {
+export class CreateBudgetsComponent {
   @Input({ required: true })
-  public year!: Number;
+  public year!: number;
 
   @Input({ required: true })
-  public budgetType!: BudgetTypeEnum;
+  public set budgetType(value: BudgetTypeEnum) {
+    this.#budgetType.next(value);
+  }
+
+  budgetService = inject(BudgetService);
+  alertService = inject(AlertService);
+  notificationService = inject(DataChangeNotificationService);
+
+  #budgetType = new BehaviorSubject<BudgetTypeEnum | null>(null);
 
   budgetTitle?: string;
   amount?: number;
 
-  availableUsers?: User[];
+  availableUsers = toSignal(
+    this.#budgetType.pipe(
+      switchMap((budgetType) =>
+        budgetType
+          ? this.budgetService.getUsersAvailableForBudgetType(budgetType)
+          : of([])
+      )
+    )
+  );
   selectedEmployee?: User | null;
 
-  constructor(
-    private budgetService: BudgetService,
-    private alertService: AlertService,
-    private notificationService: DataChangeNotificationService
-  ) {}
-
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes["year"] !== undefined || changes["budgetType"] !== undefined) {
-      this.ngOnInit();
-    }
-  }
-
-  ngOnInit(): void {
-    this.budgetService
-      .getUsersAvailableForBudgetType(this.budgetType)
-      .subscribe((users) => (this.availableUsers = users));
-  }
-
   setBudgetsForYear(): void {
-    var task = !this.selectedEmployee
+    const task = !this.selectedEmployee
       ? this.budgetService.createBudgetsForAllActiveUsers(
           this.budgetTitle!,
           this.amount!,
@@ -81,8 +58,8 @@ export class CreateBudgetsComponent implements OnInit, OnChanges {
           this.budgetType
         );
 
-    task.subscribe(
-      () => {
+    task.subscribe({
+      next: () => {
         this.alertService.success("Budget created", "addOtherBudget");
         this.notificationService.notify();
 
@@ -90,12 +67,12 @@ export class CreateBudgetsComponent implements OnInit, OnChanges {
         this.budgetTitle = undefined;
         this.selectedEmployee = null;
       },
-      (err) => {
+      error: (err) => {
         this.alertService.error(
           "Error while creating budget: " + JSON.stringify(err.error),
           "addOtherBudget"
         );
-      }
-    );
+      },
+    });
   }
 }
