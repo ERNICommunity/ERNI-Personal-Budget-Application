4bdb51ab7d520c5691fd1df302909f3ffbe9b40a
Andrej Mandli <andrej.mandli@betterask.erni>
2024-05-07 10:08:18 +0200

    msal redirect path

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
             'api://536ddfb7-294a-4065-9be5-1c580d86fd42/pba_client'
         ]
     },
-    msalLoginRedirectUri: 'https://pba.erninet.ch',
+    msalLoginRedirectUri: 'https://pba.erninet.ch/msal-redirect.html',
     msalLogoutRedirectUri: 'https://pba.erninet.ch',
     clientId: '106aed90-25b3-4d81-bbc0-9093d97668d9'
 };
