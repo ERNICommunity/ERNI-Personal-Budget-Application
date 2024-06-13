72129113b0512b71b30c2f39ca65a6de5682e144
Andrej Mandli <andrej.mandli@betterask.erni>
2024-06-13 21:10:16 +0200

    Fix mybudget

--- client/src/app/my-budget/myBudget.component.ts
+++ client/src/app/my-budget/myBudget.component.ts
@@ -1,48 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { BudgetService } from '../services/budget.service';
-import { ActivatedRoute } from '@angular/router';
-import { combineLatest } from 'rxjs';
-import { DataChangeNotificationService } from '../services/dataChangeNotification.service';
-import { MenuItem } from 'primeng/api/menuitem';
-import { map, switchMap, tap } from 'rxjs/operators';
-import { MenuHelper } from '../shared/menu-helper';
-import { CurrentUsersBudget } from '../model/current-users-budget';
+import { Component, OnInit } from "@angular/core";
+import { BudgetService } from "../services/budget.service";
+import { ActivatedRoute } from "@angular/router";
+import { combineLatest } from "rxjs";
+import { DataChangeNotificationService } from "../services/dataChangeNotification.service";
+import { MenuItem } from "primeng/api/menuitem";
+import { map, switchMap, tap } from "rxjs/operators";
+import { MenuHelper } from "../shared/menu-helper";
+import { CurrentUsersBudget } from "../model/current-users-budget";
 
 @Component({
-    selector: 'app-my-Budget',
-    templateUrl: './myBudget.component.html',
-    styleUrls: ['./myBudget.component.css']
+  selector: "app-my-Budget",
+  templateUrl: "./myBudget.component.html",
+  styleUrls: ["./myBudget.component.css"],
 })
-export class MyBudgetComponent implements OnInit {
-    budgets: CurrentUsersBudget[];
+export class MyBudgetComponent {
+  budgets = combineLatest([
+    this.route.params,
+    this.dataChangeNotificationService.notifications$,
+  ]).pipe(
+    tap(() => (this.isBusy = true)),
+    map(([params]) => +params["year"]),
+    switchMap((_) => this.budgetService.getCurrentUserBudgets(_)),
+    tap(() => (this.isBusy = false))
+  );
 
-    years: MenuItem[];
+  years: MenuItem[];
 
-    currentYear: number;
+  isBusy = false;
 
-    isBusy: boolean;
-
-    constructor(
-        private budgetService: BudgetService,
-        private route: ActivatedRoute,
-        private dataChangeNotificationService: DataChangeNotificationService
-    ) {
-        this.years = MenuHelper.getYearMenu((year) => ['/my-budget', year]);
-    }
-
-    ngOnInit() {
-        combineLatest([
-            this.route.params,
-            this.dataChangeNotificationService.notifications$
-        ])
-            .pipe(
-                tap((_) => (this.budgets = [])),
-                map(([params]) => +params['year']),
-                switchMap((_) => this.budgetService.getCurrentUserBudgets(_))
-            )
-            .subscribe((_) => {
-                this.budgets = _;
-                this.isBusy = false;
-            });
-    }
+  constructor(
+    private budgetService: BudgetService,
+    private route: ActivatedRoute,
+    private dataChangeNotificationService: DataChangeNotificationService
+  ) {
+    this.years = MenuHelper.getYearMenu((year) => ["/my-budget", year]);
+  }
 }
