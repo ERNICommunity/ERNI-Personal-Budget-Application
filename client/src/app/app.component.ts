bd5f6e1b2558c333db3966c9365ee0616a869228
Andrej Mandli <andrej.mandli@betterask.erni>
2024-06-18 23:27:11 +0200

    fixes

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,17 +1,18 @@
-import { Component } from '@angular/core';
-import { MsalBroadcastService, MsalService } from '@azure/msal-angular';
+import { Component } from "@angular/core";
+import { MsalBroadcastService, MsalService } from "@azure/msal-angular";
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-    constructor(
-        private msal: MsalService,
-        // !!! DO NOT REMOVE THIS: MsalBroadcastService has to be injected/created, the login won't work otherwise
-        private msalBroadcast: MsalBroadcastService
-    ) {
-        this.msal.instance.handleRedirectPromise();
-    }
+  constructor(
+    private msal: MsalService,
+    // !!! DO NOT REMOVE THIS: MsalBroadcastService has to be injected/created, the login won't work otherwise
+    private msalBroadcast: MsalBroadcastService
+  ) {
+    this.msal.handleRedirectObservable().subscribe();
+    //this.msal.instance.handleRedirectPromise();
+  }
 }
