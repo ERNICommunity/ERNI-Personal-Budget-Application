c4326f10067a098c4fd36c048f338a8319dbe8cc
Andrej Mandli <andrej.mandli@betterask.erni>
2024-06-18 21:36:55 +0200

    wip

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,64 +1,65 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { rootRouterConfig } from './app.routes';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
-
-import {
-    MsalInterceptor,
-    MSAL_INSTANCE,
-    MSAL_GUARD_CONFIG,
-    MSAL_INTERCEPTOR_CONFIG,
-    MsalService,
-    MsalBroadcastService
-} from '@azure/msal-angular';
+import { AppComponent } from "./app.component";
+import { RouterModule } from "@angular/router";
+import { LoginComponent } from "./login/login.component";
+import { rootRouterConfig } from "./app.routes";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
+import { ReactiveFormsModule } from "@angular/forms";
 
 import {
-    MSALGuardConfigFactory,
-    MSALInstanceFactory,
-    MSALInterceptorConfigFactory
-} from './utils/msal';
-import { SharedModule } from './shared/shared.module';
+  MsalInterceptor,
+  MSAL_INSTANCE,
+  MSAL_GUARD_CONFIG,
+  MSAL_INTERCEPTOR_CONFIG,
+  MsalService,
+  MsalBroadcastService,
+} from "@azure/msal-angular";
+
+import {
+  MSALGuardConfigFactory,
+  MSALInstanceFactory,
+  MSALInterceptorConfigFactory,
+} from "./utils/msal";
+import { SharedModule } from "./shared/shared.module";
+import { AuthenticatedComponent } from "./authenticated/authenticated.component";
 
 @NgModule({
-    declarations: [AppComponent, LoginComponent, AuthenticatedComponent],
-    imports: [
-        FormsModule,
-        HttpClientModule,
-        BrowserModule,
-        SharedModule,
-        RouterModule.forRoot(rootRouterConfig),
-        ReactiveFormsModule,
-        BrowserAnimationsModule
-    ],
-    providers: [
-        {
-            provide: HTTP_INTERCEPTORS,
-            useClass: MsalInterceptor,
-            multi: true
-        },
-        {
-            provide: MSAL_INSTANCE,
-            useFactory: MSALInstanceFactory
-        },
-        {
-            provide: MSAL_GUARD_CONFIG,
-            useFactory: MSALGuardConfigFactory
-        },
-        {
-            provide: MSAL_INTERCEPTOR_CONFIG,
-            useFactory: MSALInterceptorConfigFactory
-        },
-        MsalService,
-        MsalBroadcastService,
-    ],
-    bootstrap: [AppComponent]
+  declarations: [AppComponent, LoginComponent, AuthenticatedComponent],
+  imports: [
+    FormsModule,
+    HttpClientModule,
+    BrowserModule,
+    SharedModule,
+    RouterModule.forRoot(rootRouterConfig),
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true,
+    },
+    {
+      provide: MSAL_INSTANCE,
+      useFactory: MSALInstanceFactory,
+    },
+    {
+      provide: MSAL_GUARD_CONFIG,
+      useFactory: MSALGuardConfigFactory,
+    },
+    {
+      provide: MSAL_INTERCEPTOR_CONFIG,
+      useFactory: MSALInterceptorConfigFactory,
+    },
+    MsalService,
+    MsalBroadcastService,
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
