fa3d93307ddc2922124ef42aab64f6f3520d4a33
Andrej Mandli <andrej.mandli@betterask.erni>
2024-06-19 11:53:32 +0200

    Remove unused code

--- client/src/app/requests/requests.component.ts
+++ client/src/app/requests/requests.component.ts
@@ -1,24 +1,8 @@
-import { Component } from '@angular/core';
-import { Request } from '../model/request/request';
-import { RequestService } from '../services/request.service';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-requests',
-  templateUrl: './requests.component.html',
-  styleUrls: ['./requests.component.css']
+  selector: "app-requests",
+  templateUrl: "./requests.component.html",
+  styleUrls: ["./requests.component.css"],
 })
-export class RequestsComponent {
-  requests: Request[];
-
-  constructor(private valueService: RequestService) { }
-
-  // ngOnInit() {
-  //   this.getPendingRequests();
-  // }
-
-  // getPendingRequests(): void {
-  //   this.valueService.getPendingRequests()
-  //     .subscribe(requests => this.requests = requests);
-  // }
-
-}
+export class RequestsComponent {}
