ef3b3ec3f44b6975642999338a111c04d351b5c3
Andrej Mandli <andrej.mandli@betterask.erni>
2024-06-19 11:17:51 +0200

    Switch OtherBudgetsComponent to signals

--- client/src/app/budgets/otherBudgets/otherBudgets.component.ts
+++ client/src/app/budgets/otherBudgets/otherBudgets.component.ts
@@ -1,100 +1,84 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, computed, inject, model } from "@angular/core";
 import { BudgetService } from "../../services/budget.service";
 import { Budget } from "../../model/budget";
 import { ActivatedRoute } from "@angular/router";
-import { MenuItem } from "primeng/api/menuitem";
 import { DataChangeNotificationService } from "../../services/dataChangeNotification.service";
-import { BehaviorSubject, combineLatest, Observable } from "rxjs";
+import { combineLatest } from "rxjs";
 import { distinctUntilChanged, map, switchMap } from "rxjs/operators";
 import { MenuHelper } from "../../shared/menu-helper";
+import { toSignal } from "@angular/core/rxjs-interop";
 
 @Component({
   selector: "app-other-budgets",
   templateUrl: "./otherBudgets.component.html",
   styleUrls: ["./otherBudgets.component.css"],
 })
-export class OtherBudgetsComponent implements OnInit {
-  years$: Observable<MenuItem[]>;
-  budgetTypes$: Observable<MenuItem[]>;
+export class OtherBudgetsComponent {
+  budgetService = inject(BudgetService);
+  route = inject(ActivatedRoute);
+  notificationService = inject(DataChangeNotificationService);
 
-  budgets$: Observable<Budget[]>;
+  selectedYear$ = this.route.params.pipe(
+    map((params) => +params["year"]),
+    distinctUntilChanged()
+  );
 
-  disableSetOrEditBudgets$: Observable<boolean>;
-
-  selectedYear$: Observable<number>;
-  selectedBudgetType$: Observable<number>;
-
-  private searchTerm$ = new BehaviorSubject<string | null>(null);
-
-  get searchTerm(): string | null {
-    return this.searchTerm$.value;
-  }
-
-  set searchTerm(value: string) {
-    this.searchTerm$.next(value);
-  }
-
-  constructor(
-    private budgetService: BudgetService,
-    private route: ActivatedRoute,
-    private notificationService: DataChangeNotificationService
-  ) {}
-
-  ngOnInit() {
-    this.selectedYear$ = this.route.params.pipe(
-      map((params) => +params["year"]),
-      distinctUntilChanged()
-    );
-
-    this.selectedBudgetType$ = this.route.params.pipe(
+  selectedBudgetType = toSignal(
+    this.route.params.pipe(
       map((params) => +params["budgetType"]),
       distinctUntilChanged()
-    );
+    ),
+    { initialValue: undefined }
+  );
 
-    const currentYear = new Date().getFullYear();
+  years = computed(() => {
+    const selectedBudgetType = this.selectedBudgetType();
+    return selectedBudgetType
+      ? MenuHelper.getYearMenu((year) => [
+          "/budgets/",
+          year,
+          selectedBudgetType,
+        ])
+      : [];
+  });
 
-    this.disableSetOrEditBudgets$ = this.selectedYear$.pipe(
-      map(
-        (selectedYear) =>
-          selectedYear === currentYear || selectedYear === currentYear + 1
-      )
-    );
+  budgetTypes$ = combineLatest([
+    this.budgetService.getBudgetsTypes(),
+    this.selectedYear$,
+  ]).pipe(
+    map(([types, year]) =>
+      types.map((type) => ({
+        label: type.name,
+        routerLink: ["/budgets/", year, type.id],
+      }))
+    )
+  );
 
-    this.years$ = this.selectedBudgetType$.pipe(
-      map((budgetType) =>
-        MenuHelper.getYearMenu((year) => ["/budgets/", year, budgetType])
-      )
-    );
-
-    const budgets$ = combineLatest([
+  #budgets = toSignal(
+    combineLatest([
       this.selectedYear$,
       this.notificationService.notifications$,
     ]).pipe(
       switchMap(([year]) => this.budgetService.getActiveUsersBudgets(year))
-    );
+    ),
+    { initialValue: [] }
+  );
 
-    this.budgets$ = combineLatest([
-      budgets$,
-      this.searchTerm$,
-      this.selectedBudgetType$,
-    ]).pipe(
-      map(([budgets, searchTerm, budgetType]) =>
-        this.filterBudgets(budgets, searchTerm ?? "", budgetType)
-      )
-    );
+  budgets = computed(() => {
+    const selectedBudgetType = this.selectedBudgetType();
 
-    this.budgetTypes$ = combineLatest([
-      this.budgetService.getBudgetsTypes(),
-      this.selectedYear$,
-    ]).pipe(
-      map(([types, year]) =>
-        types.map((type) => ({
-          label: type.name,
-          routerLink: ["/budgets/", year, type.id],
-        }))
-      )
+    if (!selectedBudgetType) {
+      return [];
+    }
+
+    return this.filterBudgets(
+      this.#budgets(),
+      this.searchTerm(),
+      selectedBudgetType
     );
-  }
+  });
+
+  searchTerm = model("");
 
   filterBudgets(budgets: Budget[], searchString: string, budgetType: number) {
     searchString = searchString ?? "";
