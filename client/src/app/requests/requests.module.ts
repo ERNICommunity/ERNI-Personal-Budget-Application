d0b8dd3cee11ccb05853667a3bd236f93a0409d6
Andrej Mandli <andrej.mandli@betterask.erni>
2024-07-18 12:20:01 +0200

    Fix image upload

--- client/src/app/requests/requests.module.ts
+++ client/src/app/requests/requests.module.ts
@@ -1,68 +1,62 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { RequestsComponent } from './requests.component';
-import { ViewerGuard } from '../services/guards/viewer.guard';
-import { RequestDetailComponent } from './requestDetail/requestDetail.component';
-import { RequestEditComponent } from './requestEdit/requestEdit.component';
-import { RequestListComponent } from './requestList/requestList.component';
-import { FormsModule } from '@angular/forms';
-import { SharedModule } from '../shared/shared.module';
-import { BasicRequestInfoEditorComponent } from './requestEdit/basic-request-info-editor/basic-request-info-editor.component';
-import { RequestMassComponent } from './requestMass/requestMass.component';
-import { AdminRoleGuard } from '../services/guards/admin-role.guard';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+import { RequestsComponent } from "./requests.component";
+import { ViewerGuard } from "../services/guards/viewer.guard";
+import { RequestDetailComponent } from "./requestDetail/requestDetail.component";
+import { RequestEditComponent } from "./requestEdit/requestEdit.component";
+import { RequestListComponent } from "./requestList/requestList.component";
+import { FormsModule } from "@angular/forms";
+import { SharedModule } from "../shared/shared.module";
+import { RequestMassComponent } from "./requestMass/requestMass.component";
+import { AdminRoleGuard } from "../services/guards/admin-role.guard";
 
 @NgModule({
-    declarations: [
-        RequestsComponent,
-        BasicRequestInfoEditorComponent,
-        RequestEditComponent,
-        RequestListComponent,
-        RequestDetailComponent,
-        RequestMassComponent,
-        BasicRequestInfoEditorComponent
-    ],
-    exports: [BasicRequestInfoEditorComponent],
-    imports: [
-        CommonModule,
-        FormsModule,
-        SharedModule,
-        RouterModule.forChild([
-            {
-                path: '',
-                component: RequestsComponent,
+  declarations: [
+    RequestsComponent,
+    RequestListComponent,
+    RequestDetailComponent,
+    RequestMassComponent,
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    SharedModule,
+    RouterModule.forChild([
+      {
+        path: "",
+        component: RequestsComponent,
+        canActivate: [ViewerGuard],
+        children: [
+          {
+            path: "",
+            redirectTo: "1/pending/" + new Date().getFullYear().toString(),
+            pathMatch: "full",
+          },
+          {
+            path: ":budgetType/:requestState/:year",
+            component: RequestListComponent,
+            canActivate: [ViewerGuard],
+            children: [
+              {
+                path: "detail/:requestId",
+                component: RequestDetailComponent,
                 canActivate: [ViewerGuard],
-                children: [
-                    {
-                        path: '',
-                        redirectTo:
-                            '1/pending/' + new Date().getFullYear().toString(),
-                        pathMatch: 'full'
-                    },
-                    {
-                        path: ':budgetType/:requestState/:year',
-                        component: RequestListComponent,
-                        canActivate: [ViewerGuard],
-                        children: [
-                            {
-                                path: 'detail/:requestId',
-                                component: RequestDetailComponent,
-                                canActivate: [ViewerGuard]
-                            },
-                            {
-                                path: 'edit/:id',
-                                component: RequestEditComponent
-                            }
-                        ]
-                    },
-                    {
-                        path: 'mass-request',
-                        component: RequestMassComponent,
-                        canActivate: [AdminRoleGuard]
-                    }
-                ]
-            }
-        ])
-    ]
+              },
+              {
+                path: "edit/:id",
+                component: RequestEditComponent,
+              },
+            ],
+          },
+          {
+            path: "mass-request",
+            component: RequestMassComponent,
+            canActivate: [AdminRoleGuard],
+          },
+        ],
+      },
+    ]),
+  ],
 })
 export class RequestsModule {}
