95b2ac326d2709624619c27ae28ab256c19db9c2
Peter Vaclavek <P.Vaclavek-extern@bruker.com>
2024-07-25 13:24:18 +0200

    better handling of incorrect year router params

--- client/src/app/my-budget/myBudget.component.ts
+++ client/src/app/my-budget/myBudget.component.ts
@@ -1,37 +1,35 @@
-import { Component, inject } from "@angular/core";
-import { BudgetService } from "../services/budget.service";
-import { ActivatedRoute } from "@angular/router";
-import { combineLatest } from "rxjs";
-import { DataChangeNotificationService } from "../services/dataChangeNotification.service";
-import { map, switchMap, tap } from "rxjs/operators";
-import { MenuHelper } from "../shared/menu-helper";
-import { toSignal } from "@angular/core/rxjs-interop";
+import {Component, inject, signal} from "@angular/core";
+import {BudgetService} from "../services/budget.service";
+import {ActivatedRoute} from "@angular/router";
+import {combineLatest} from "rxjs";
+import {DataChangeNotificationService} from "../services/dataChangeNotification.service";
+import {filter, map, switchMap, tap} from "rxjs/operators";
+import {MenuHelper} from "../shared/menu-helper";
+import {toSignal} from "@angular/core/rxjs-interop";
 
 @Component({
   selector: "app-my-budget",
   templateUrl: "./myBudget.component.html",
-  styleUrls: ["./myBudget.component.css"],
 })
 export class MyBudgetComponent {
   #budgetService = inject(BudgetService);
   #route = inject(ActivatedRoute);
   #dataChangeNotificationService = inject(DataChangeNotificationService);
 
+  isBusy = signal(false);
   budgets = toSignal(
     combineLatest([
       this.#route.params,
       this.#dataChangeNotificationService.notifications$,
     ]).pipe(
-      tap((_) => (this.isBusy = true)),
-      map(([params]) => +params["year"]),
-      switchMap((_) => this.#budgetService.getCurrentUserBudgets(_)),
-      tap((_) => (this.isBusy = false))
-    )
+      map(([params]) => Number(params["year"])),
+      filter((year) => !isNaN(year)),
+      tap((_) => (this.isBusy.set(true))),
+      switchMap((year) => this.#budgetService.getCurrentUserBudgets(year)),
+      tap((_) => (this.isBusy.set(false)))
+    ),
+    { initialValue: [] }
   );
-
-  currentYear: number;
-
-  isBusy: boolean;
 
   years = MenuHelper.getYearMenu((year) => ["/my-budget", year]);
 }
