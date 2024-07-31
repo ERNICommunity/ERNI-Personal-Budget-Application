f56cc8820fd92365b533e19fe0e645a5f575d6f5
Peter Vaclavek <P.Vaclavek-extern@bruker.com>
2024-07-31 12:13:27 +0200

    add easter-egg to My-Budget section
    - triggers confetti animation when clicking on the budget progress bar
    - number of used confetti is related to the amount of the budget

---
+++ client/src/app/shared/directives/money-confetti.directive.ts
@@ -0,0 +1,88 @@
+import {
+  Directive,
+  inject,
+} from "@angular/core";
+import { ProgressBar } from "primeng/progressbar";
+
+@Directive({
+  selector: "p-progressBar[pbaMoneyConfetti]",
+  standalone: true,
+  host: {
+    "(click)": "onClick($event)"
+  },
+})
+export class MoneyConfettiDirective {
+  #progressBar = inject(ProgressBar);
+
+  onClick(event: MouseEvent): void {
+    const progressValue = this.#progressBar.value ?? 0;
+    const confettiCount = 20 * progressValue / 100;
+    if (confettiCount <= 0) {
+      return;
+    }
+
+    // create necessary HTML elements
+    this.createStyleElement();
+    const container = this.createContainerElement(event.pageX, event.pageY);
+    this.createConfettiElements(container, confettiCount);
+
+    // attach elements to the DOM and remove them after some delay
+    document.body.appendChild(container)
+    setTimeout(() => container.remove(), 700)
+  }
+
+  private createConfettiElements(container: HTMLDivElement, confettiCount: number) {
+    for (let i = 0; i < confettiCount; i++) {
+      const styles = `
+        transform: translate3d(${this.random(500) - 250}px, ${this.random(200) - 150}px, 0) rotate(${this.random(360)}deg);
+      `
+      const confettiEl = document.createElement("div");
+      confettiEl.textContent = '💵'
+      confettiEl.style.cssText = styles;
+      confettiEl.classList.add('confetti')
+      container.appendChild(confettiEl);
+    }
+  }
+
+  private createContainerElement(xPos: number, yPos: number) {
+    const container = document.createElement('div');
+    container.style.position = 'absolute';
+    container.style.left = `${xPos}px`;
+    container.style.top = `${yPos}px`;
+    return container;
+  }
+
+  private createStyleElement(): void {
+    let styleEl = document.getElementById('confetti-style') as HTMLStyleElement;
+    if (styleEl) {
+      // our style element is already attached to the DOM, no need to create it
+      return;
+    }
+
+    const headEl = document.getElementsByTagName('head')[0];
+    styleEl = document.createElement('style');
+    styleEl.id = 'confetti-style';
+
+    const css = `
+      @keyframes bang {
+        from {
+          transform: translate3d(0,0,0);
+          opacity: 1;
+        }
+      }
+
+      .confetti {
+        position: absolute;
+        opacity: 0;
+        pointer-events: none;
+        animation: bang 700ms ease-out forwards;
+      }
+    `;
+    styleEl.appendChild(document.createTextNode(css));
+    headEl.appendChild(styleEl);
+  }
+
+  private random(max: number): number{
+    return Math.random() * max;
+  }
+}
