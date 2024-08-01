edec53b78860b1a66b072bd3f739e9d4c8756a55
Peter Vaclavek <P.Vaclavek-extern@bruker.com>
2024-08-01 15:24:47 +0200

    redesign file upload component
    - add support for file drag & drop
    - improve UI/UX of uploaded files
    - fix a11y issues
    - removed obsolete basic-request-info-editor component

--- client/src/app/requests/requestEdit/requestEdit.component.ts
+++ client/src/app/requests/requestEdit/requestEdit.component.ts
@@ -11,19 +11,18 @@ import { ActivatedRoute } from '@angular/router';
 import { RequestApprovalState } from '../../model/requestState';
 import { InvoiceImageService } from '../../services/invoice-image.service';
 import { FileListComponent, Invoice, NewInvoiceStatus } from '../../shared/file-list/file-list.component';
-import { concatMap, defaultIfEmpty, map } from 'rxjs/operators';
-import { forkJoin, Observable, Subject } from 'rxjs';
+import { concatMap, defaultIfEmpty, map, tap } from 'rxjs/operators';
+import { finalize, forkJoin, Observable, Subject } from 'rxjs';
 import { InvoiceImage } from '../../model/InvoiceImage';
 import { Router } from '@angular/router';
 import { SharedModule } from '../../shared/shared.module';
-import { BasicRequestInfoEditorComponent } from './basic-request-info-editor/basic-request-info-editor.component';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-request-edit',
   templateUrl: 'requestEdit.component.html',
   standalone: true,
-  imports: [SharedModule, BasicRequestInfoEditorComponent, FileListComponent],
+  imports: [SharedModule, FileListComponent],
 })
 export class RequestEditComponent {
   // route params as inputs
@@ -34,7 +33,8 @@ export class RequestEditComponent {
   budgetType = signal<BudgetTypeEnum | null>(null);
   request = signal<Request | null>(null);
   isSaveInProgress = signal(false);
-  files = signal<Invoice[]>([]);
+  attachments = signal<Invoice[]>([]);
+  isRequestLoading = signal(false);
 
   isNewRequest = computed(() => !!this.budgetId());
 
@@ -57,7 +57,7 @@ export class RequestEditComponent {
       untracked(() => {
         if (budgetId && !isNaN(budgetId)) {
           this.request.set(this.createNewRequest());
-          this.files.set([]);
+          this.attachments.set([]);
         } else if (requestId && !isNaN(requestId)) {
           this.loadRequest(requestId);
         } else {
@@ -81,51 +81,58 @@ export class RequestEditComponent {
   }
 
   private loadRequest(requestId: number): void {
-    this.requestService
-      .getRequest(requestId)
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe((request) => this.request.set(request));
+    this.isRequestLoading.set(true);
 
-    this.invoiceImageService
-      .getInvoiceImages(requestId)
+    const request$ = this.requestService.getRequest(requestId).pipe(tap((request) => this.request.set(request)));
+    const attachments$ = this.invoiceImageService.getInvoiceImages(requestId).pipe(
+      map((names) =>
+        names.map<Invoice>((invoice) => ({
+          name: invoice.name,
+          status: { code: 'saved', id: invoice.id },
+        })),
+      ),
+      tap((attachments) => this.attachments.set(attachments)),
+    );
+
+    forkJoin({
+      request: request$,
+      attachments: attachments$,
+    })
       .pipe(
+        finalize(() => this.isRequestLoading.set(false)),
         takeUntilDestroyed(this.destroyRef),
-        map((names) =>
-          names.map<Invoice>((invoice) => ({
-            name: invoice.name,
-            status: { code: 'saved', id: invoice.id },
-          })),
-        ),
       )
-      .subscribe((files) => this.files.set(files));
+      .subscribe();
   }
 
-  public onNewImageAdded(files: File[]) {
+  public onNewFileAdded(files: File[]) {
     for (let i = 0; i < files.length; i++) {
       const selectedFile = files[i];
 
-      const im: Invoice = {
+      const attachment: Invoice = {
         status: { code: 'new', file: selectedFile },
         name: selectedFile.name,
       };
-      this.files.update((files) => [...files, im]);
+      this.attachments.update((files) => [...files, attachment]);
 
       const requestId = this.requestId();
       if (requestId) {
-        this.uploadInvoice(requestId, im, selectedFile);
+        this.uploadAttachment(requestId, attachment, selectedFile);
       }
     }
   }
 
-  private uploadInvoices(requestId: number): Observable<number[] | null> {
-    const uploads$ = this.files()
-      .filter((invoice): invoice is { status: NewInvoiceStatus; name: string } => invoice.status.code === 'new')
-      .map((invoice) => this.uploadInvoice(requestId, invoice, invoice.status.file));
+  private uploadAttachments(requestId: number): Observable<number[] | null> {
+    const uploads$ = this.attachments()
+      .filter(
+        (attachment): attachment is { status: NewInvoiceStatus; name: string } => attachment.status.code === 'new',
+      )
+      .map((attachment) => this.uploadAttachment(requestId, attachment, attachment.status.file));
 
     return forkJoin(uploads$).pipe(defaultIfEmpty(null));
   }
 
-  private uploadInvoice(requestId: number, invoice: Invoice, file: File): Observable<number> {
+  private uploadAttachment(requestId: number, attachment: Invoice, file: File): Observable<number> {
     const result = new Subject<number>();
 
     const fileReader = new FileReader();
@@ -135,26 +142,26 @@ export class RequestEditComponent {
         const payload: InvoiceImage = {
           requestId: requestId,
           data: fileReader.result.toString().replace('data:', '').replace(/^.+,/, ''),
-          filename: invoice.name,
+          filename: attachment.name,
           mimeType: file.type,
         };
 
-        this.updateFiles({
-          ...invoice,
+        this.updateAttachmentState({
+          ...attachment,
           status: { code: 'in-progress', progress: 0 },
         });
 
         const uploadInfo = this.invoiceImageService.addInvoiceImage(payload);
 
         uploadInfo.progress.subscribe((progress) =>
-          this.updateFiles({
-            ...invoice,
+          this.updateAttachmentState({
+            ...attachment,
             status: { code: 'in-progress', progress },
           }),
         );
         uploadInfo.id.subscribe((id) =>
-          this.updateFiles({
-            ...invoice,
+          this.updateAttachmentState({
+            ...attachment,
             status: { code: 'saved', id },
           }),
         );
@@ -207,7 +214,7 @@ export class RequestEditComponent {
         ? this.requestService.addTeamRequest(payload)
         : this.requestService.addRequest(payload);
 
-    request$.pipe(concatMap((requestId) => this.uploadInvoices(requestId))).subscribe({
+    request$.pipe(concatMap((requestId) => this.uploadAttachments(requestId))).subscribe({
       next: (_) => {
         this.isSaveInProgress.set(false);
         this.dataChangeNotificationService.notify();
@@ -258,7 +265,13 @@ export class RequestEditComponent {
     this.router.navigate(['../../'], { relativeTo: this.route });
   }
 
-  private updateFiles(fileToUpdate: Invoice): void {
-    this.files.update((files) => [...files.map((file) => (file.name !== fileToUpdate.name ? file : fileToUpdate))]);
+  /**
+   * Updates local storage of list of attachments with updated attachment.
+   * @param updatedAttachment
+   */
+  private updateAttachmentState(updatedAttachment: Invoice): void {
+    this.attachments.update((_) => [
+      ..._.map((attachment) => (attachment.name !== updatedAttachment.name ? attachment : updatedAttachment)),
+    ]);
   }
 }
